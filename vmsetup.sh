8193ccc53c4b665b46d569677e4f42cf4e2924cc
Pradyumna Nagendra <pradyumnan@ilimi.in>
2019-11-15 17:58:34 +0530

    Issue #000 feat: Updated circleci config

--- vmsetup.sh
+++ vmsetup.sh
@@ -5,9 +5,8 @@ curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.2
 sudo dpkg -i elasticsearch-6.3.2.deb
 sudo service elasticsearch start
 sudo service elasticsearch status
-mvn install:install-file -Dfile=./orchestrator/module/interpreter-api/lib/jtcl-2.7.0.jar -DgroupId=tcl.lang -DartifactId=jtcl -Dversion=2.7.0 -Dpackaging=jar
+sudo mkdir /data
 mkdir -p ~/logs/search/
-find ./ -type f -name "log4j2.xml" -print0 | xargs -0 sed -i -e 's/\/data\/logs/~\/logs/g'
 find ./ -type f -name "logback.xml" -print0 | xargs -0 sed -i -e 's/\/data\/logs/logs/g'
 find ./ -type f -name "application.conf" -print0 | xargs -0 sed -i -e 's/\/data\//~\//g'
 find ./ -type f -name "*.java" -print0 | xargs -0 sed -i -e 's/\/data\//~\//g'
