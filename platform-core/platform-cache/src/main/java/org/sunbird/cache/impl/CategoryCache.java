9a96f80b0fb65b5e26f29da02d298e0aa1338dd5
Mahesh Kumar Gangula <mahesh@ilimi.in>
2019-11-28 14:53:03 +0530

    Issue #000 fix: null pointer exception.

--- platform-core/platform-cache/src/main/java/org/sunbird/cache/impl/CategoryCache.java
+++ platform-core/platform-cache/src/main/java/org/sunbird/cache/impl/CategoryCache.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class CategoryCache extends RedisCacheManager {
 
     public CategoryCache(){
-        handler = new CategoryCacheHandler();
+//        handler = new CategoryCacheHandler();
     }
 
     @Override
