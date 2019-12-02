ea83665c7de202bbadd2a46db4c3504dc8715aaa
Pradyumna Nagendra <pradyumnan@ilimi.in>
2019-12-02 18:46:01 +0530

    Issue #000 feat: Added unit tests to data node

--- ontology-engine/graph-engine_2.11/src/test/scala/org/sunbird/graph/nodes/TestDataNode.scala
+++ ontology-engine/graph-engine_2.11/src/test/scala/org/sunbird/graph/nodes/TestDataNode.scala
@@ -3,7 +3,7 @@ package org.sunbird.graph.nodes
 import java.util
 
 import org.sunbird.common.dto.Request
-import org.sunbird.common.exception.ResourceNotFoundException
+import org.sunbird.common.exception.{ClientException, ResourceNotFoundException}
 import org.sunbird.graph.BaseSpec
 import org.sunbird.graph.dac.model.Node
 
@@ -80,11 +80,23 @@ class TestDataNode extends BaseSpec {
         request.put("channel", "in.ekstep")
         request.put("body", "body")
         val future: Future[Node] = DataNode.create(request)
-        future map {node => {assert(null != node)
+        future map { node => {
+            assert(null != node)
             print(node)
-            assert(node.getMetadata.get("name").asInstanceOf[String].equalsIgnoreCase("testResource"))}}
-    }
+            assert(node.getMetadata.get("name").asInstanceOf[String].equalsIgnoreCase("testResource"))
+        }
+            val req = new Request(request)
+            req.put("identifier", node.getIdentifier)
+            req.put("fields", util.Arrays.asList("body"))
+            val readFuture = DataNode.read(req)
+            readFuture map { node => {
+                assert(node.getMetadata.get("name").asInstanceOf[String].equalsIgnoreCase("testResource"))
+                assert(node.getMetadata.get("body").asInstanceOf[String].equalsIgnoreCase("body"))
+            }
+            }
 
+        } flatMap (f => f)
+    }
 
     "createNode with invalid relation" should "throw resource not found exception" in  {
         val request = new Request()
@@ -105,4 +117,143 @@ class TestDataNode extends BaseSpec {
         recoverToSucceededIf[ResourceNotFoundException](DataNode.create(request))
     }
 
+    "update content with valid data" should "update node" in {
+        val request = new Request()
+        request.setObjectType("Content")
+        request.setContext(getContextMap())
+
+        request.put("code", "test")
+        request.put("name", "testResource")
+        request.put("mimeType", "application/pdf")
+        request.put("contentType", "Resource")
+        request.put("description", "test")
+        request.put("channel", "in.ekstep")
+        val future: Future[Node] = DataNode.create(request)
+        future map {node => {assert(null != node)
+            print(node)
+            assert(node.getMetadata.get("name").asInstanceOf[String].equalsIgnoreCase("testResource"))
+            val req = new Request(request)
+            req.getContext.put("identifier", node.getIdentifier)
+            req.put("name", "updated name")
+            val updateFuture = DataNode.update(req)
+            updateFuture map { node => {
+                    assert(node.getMetadata.get("name").asInstanceOf[String].equalsIgnoreCase("updated name"))
+                }
+            }
+            }
+        } flatMap(f => f)
+    }
+
+    "update content with valid relation" should "update node with relation" in {
+        val request = new Request()
+        request.setObjectType("Content")
+        request.setContext(getContextMap())
+
+        request.put("code", "test")
+        request.put("name", "testResource")
+        request.put("mimeType", "application/pdf")
+        request.put("contentType", "Resource")
+        request.put("description", "test")
+        request.put("channel", "in.ekstep")
+        val future: Future[Node] = DataNode.create(request)
+        future map {node => {assert(null != node)
+            print(node)
+            assert(node.getMetadata.get("name").asInstanceOf[String].equalsIgnoreCase("testResource"))
+            val req = new Request(request)
+            req.getContext.put("identifier", node.getIdentifier)
+            req.put("name", "updated name")
+            req.put("concepts", new util.ArrayList[util.Map[String, AnyRef]](){{
+                add(new util.HashMap[String, AnyRef](){{
+                    put("identifier", "Num:C3:SC2")
+                }})
+            }})
+            val updateFuture = DataNode.update(req)
+            updateFuture map { node => {
+                assert(node.getMetadata.get("name").asInstanceOf[String].equalsIgnoreCase("updated name"))
+                assert(node.getOutRelations.get(0).getEndNodeId().equalsIgnoreCase("Num:C3:SC2"))
+            }
+            }
+        }
+        } flatMap(f => f)
+    }
+
+    "update content with invalid versionKey" should "throw client exception" in {
+        val request = new Request()
+        request.setObjectType("Content")
+        request.setContext(getContextMap())
+
+        request.put("code", "test")
+        request.put("name", "testResource")
+        request.put("mimeType", "application/pdf")
+        request.put("contentType", "Resource")
+        request.put("description", "test")
+        request.put("channel", "in.ekstep")
+        val future: Future[Node] = DataNode.create(request)
+        future map { node => {
+            assert(null != node)
+            print(node)
+            assert(node.getMetadata.get("name").asInstanceOf[String].equalsIgnoreCase("testResource"))
+            val req = new Request(request)
+            req.getContext.put("identifier", node.getIdentifier)
+            req.put("name", "updated name")
+            req.put("versionKey", "invalidVersionKey")
+            recoverToSucceededIf[ClientException](DataNode.update(req))
+        }
+        }flatMap(f => f)
+    }
+
+    "update content with invalid relation" should "update node with relation" in {
+        val request = new Request()
+        request.setObjectType("Content")
+        request.setContext(getContextMap())
+
+        request.put("code", "test")
+        request.put("name", "testResource")
+        request.put("mimeType", "application/pdf")
+        request.put("contentType", "Resource")
+        request.put("description", "test")
+        request.put("channel", "in.ekstep")
+        val future: Future[Node] = DataNode.create(request)
+        future map {node => {assert(null != node)
+            print(node)
+            assert(node.getMetadata.get("name").asInstanceOf[String].equalsIgnoreCase("testResource"))
+            val req = new Request(request)
+            req.getContext.put("identifier", node.getIdentifier)
+            req.put("name", "updated name")
+            req.put("concepts", new util.ArrayList[util.Map[String, AnyRef]](){{
+                add(new util.HashMap[String, AnyRef](){{
+                    put("identifier", "invalidId")
+                }})
+            }})
+            recoverToSucceededIf[ResourceNotFoundException](DataNode.update(req))
+        }
+        } flatMap(f => f)
+    }
+
+    "listNode" should "lists a node successfully" in {
+        val request = new Request()
+        request.setObjectType("Content")
+        request.setContext(getContextMap())
+
+        request.put("code", "test")
+        request.put("name", "testResource")
+        request.put("mimeType", "application/pdf")
+        request.put("contentType", "Resource")
+        request.put("description", "test")
+        request.put("channel", "in.ekstep")
+        val future: Future[Node] = DataNode.create(request)
+        future map {node => {assert(null != node)
+            print(node)
+            assert(node.getMetadata.get("name").asInstanceOf[String].equalsIgnoreCase("testResource"))}
+            val req = new Request(request)
+            req.put("identifiers", new util.ArrayList[String](){{ add(node.getIdentifier)}})
+            val listFuture = DataNode.list(req)
+            listFuture map { nodes => {
+                assert(!nodes.isEmpty)
+                assert(nodes.get(0).getIdentifier.equalsIgnoreCase(node.getIdentifier))
+            }
+
+            }
+        }flatMap(f => f)
+    }
 }
