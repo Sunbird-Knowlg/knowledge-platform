5a02c41ae63958848b80194c0d598e63635d5bac
Mahesh Kumar Gangula <mahesh@ilimi.in>
2019-12-12 20:57:30 +0530

    Issue #000 feat: SIGTERM handler implementation.

--- learning-api/content-service/app/controllers/HealthController.scala
+++ learning-api/content-service/app/controllers/HealthController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import akka.actor.{ActorRef, ActorSystem}
+import handlers.SignalHandler
 import javax.inject._
 import play.api.mvc._
 import utils.{ActorNames, ApiId}
@@ -8,9 +9,14 @@ import utils.{ActorNames, ApiId}
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future, Promise}
 
-class HealthController @Inject()(@Named(ActorNames.HEALTH_ACTOR) healthActor: ActorRef, cc: ControllerComponents, actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends BaseController(cc) {
+class HealthController @Inject()(@Named(ActorNames.HEALTH_ACTOR) healthActor: ActorRef, cc: ControllerComponents, actorSystem: ActorSystem, signalHandler: SignalHandler)(implicit exec: ExecutionContext) extends BaseController(cc) {
 
     def health() = Action.async { implicit request =>
-        getResult(ApiId.APPLICATION_HEALTH, healthActor, new org.sunbird.common.dto.Request())
+        if (signalHandler.isShuttingDown) {
+            Future { ServiceUnavailable }
+        } else {
+            getResult(ApiId.APPLICATION_HEALTH, healthActor, new org.sunbird.common.dto.Request())
+        }
+
     }
 }
