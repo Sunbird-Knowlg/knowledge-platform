051393993da80ca64519e4cd1957bfbae26ec1c6
Kumar Gauraw <er.krgauraw@gmail.com>
2019-12-19 15:10:24 +0530

    Issue #SC-0000 feat: Added assessment-service

--- build/build.sh
+++ build/build.sh
@@ -3,9 +3,9 @@
 set -eo pipefail
 
 build_tag=$1
-name=content-service
-node=$2
-org=$3
+name=$2
+node=$3
+org=$4
 
-docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
+docker build -f build/${name}/Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
