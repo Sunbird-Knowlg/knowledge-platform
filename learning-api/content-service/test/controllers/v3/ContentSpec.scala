4a7a35140cf7a9a00db3535e34482aabf29131f2
Mahesh Kumar Gangula <mahesh@ilimi.in>
2019-12-31 14:51:18 +0530

    Issue #000 fix: content controller testcases.

--- learning-api/content-service/test/controllers/v3/ContentSpec.scala
+++ learning-api/content-service/test/controllers/v3/ContentSpec.scala
@@ -11,9 +11,37 @@ import play.api.test.Helpers._
 class ContentSpec  extends BaseSpec {
 
     "Content Controller " should {
+        "return success response for create API" in {
+            val controller = app.injector.instanceOf[controllers.v3.ContentController]
+            val result = controller.create()(FakeRequest())
+            isOK(result)
+            status(result) must equalTo(OK)
+        }
+
+        "return success response for update API" in {
+            val controller = app.injector.instanceOf[controllers.v3.ContentController]
+            val result = controller.update("do_123")(FakeRequest())
+            isOK(result)
+            status(result) must equalTo(OK)
+        }
+
         "return success response for read API" in {
             val controller = app.injector.instanceOf[controllers.v3.ContentController]
             val result = controller.read("do_123", None, None)(FakeRequest())
+            isOK(result)
+            status(result) must equalTo(OK)
+        }
+
+        "return success response for hierarchy add API" in {
+            val controller = app.injector.instanceOf[controllers.v3.ContentController]
+            val result = controller.addHierarchy()(FakeRequest())
+            isOK(result)
+            status(result) must equalTo(OK)
+        }
+
+        "return success response for hierarchy remove API" in {
+            val controller = app.injector.instanceOf[controllers.v3.ContentController]
+            val result = controller.addHierarchy()(FakeRequest())
             isOK(result)
             status(result) must equalTo(OK)
         }
