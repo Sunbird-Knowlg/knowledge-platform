3cf331f85a82e29dcdca54be2aa1bed22c725252
Mahesh Kumar Gangula <mahesh@ilimi.in>
2020-01-02 09:57:56 +0530

    Issue #000 fix: separate badrequest spec.

--- learning-api/content-service/test/controllers/v3/HealthSpec.scala
+++ learning-api/content-service/test/controllers/v3/HealthSpec.scala
@@ -7,11 +7,8 @@ import play.api.test.Helpers._
 import play.api.test._
 
 @RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends BaseSpec {
+class HealthSpec extends BaseSpec {
     "Application" should {
-        "send 404 on a bad request for /boum" in {
-            route(app, FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
-        }
         "return api health status report - successful" in {
             val controller = app.injector.instanceOf[controllers.HealthController]
             val result = controller.health()(FakeRequest())
