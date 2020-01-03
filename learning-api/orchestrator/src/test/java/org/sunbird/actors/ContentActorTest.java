1fe25e87b82f870da633ca79cfd712c91a97fb58
Mahesh Kumar Gangula <mahesh@ilimi.in>
2020-01-03 09:51:46 +0530

    Issue #000 fix: readContent test case.

--- learning-api/orchestrator/src/test/java/org/sunbird/actors/ContentActorTest.java
+++ learning-api/orchestrator/src/test/java/org/sunbird/actors/ContentActorTest.java
@@ -3,16 +3,25 @@ package org.sunbird.actors;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.dispatch.Futures;
 import akka.testkit.TestKit;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.sunbird.common.dto.Request;
 import org.sunbird.common.dto.Response;
+import org.sunbird.graph.dac.model.Node;
 import org.sunbird.graph.nodes.DataNode;
+import scala.concurrent.ExecutionContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({
@@ -33,8 +42,39 @@ public class ContentActorTest {
         Assert.assertTrue("failed".equals(response.getParams().getStatus()));
     }
 
-//    @Test
-//    public void createContent() {
-//        PowerMockito.mockStatic(DataNode.class);
-//    }
+    @Test
+    public void readContent() throws Exception {
+        PowerMockito.mockStatic(DataNode.class);
+        String identifier = "do_1234";
+        when(DataNode.read(Mockito.any(Request.class), Mockito.any(ExecutionContext.class))).thenReturn(Futures.successful(getNode(identifier)));
+        Request request = getRequest("readContent");
+        request.put("fields", "");
+
+        TestKit probe = new TestKit(system);
+        ActorRef actorRef = system.actorOf(props);
+        actorRef.tell(request, probe.testActor());
+        Response response = probe.expectMsgClass(Response.class);
+        Assert.assertTrue("successful".equals(response.getParams().getStatus()));
+        Assert.assertTrue(null != response.getResult().get("content"));
+        Map<String, Object> content = (Map<String, Object>) response.getResult().get("content");
+        Assert.assertTrue(identifier.equals(content.get("identifier")));
+
+    }
+
+    private Request getRequest(String operation) {
+        Request request = new Request();
+        request.setContext(new HashMap<String, Object>() {{
+            put("schemaName", "content");
+        }});
+        request.setOperation(operation);
+        return request;
+    }
+
+    private Node getNode(String identifier) {
+        Node node = new Node();
+        node.setGraphId("domain");
+        node.setIdentifier(identifier);
+        node.setMetadata(new HashMap<>());
+        return node;
+    }
 }
