e034d77b524786a0b2e83cc841feae52e6d0b1ce
Kumar Gauraw <er.krgauraw@gmail.com>
2020-01-07 12:24:31 +0530

    Issue #SC-0000 fix: updated code as per review comment

--- assessment-api/assessment-actors/src/main/scala/org/sunbird/actors/ItemSetActor.scala
+++ assessment-api/assessment-actors/src/main/scala/org/sunbird/actors/ItemSetActor.scala
@@ -9,6 +9,7 @@ import org.sunbird.common.dto.{Request, Response, ResponseHandler}
 import org.sunbird.graph.dac.model.Relation
 import org.sunbird.graph.nodes.DataNode
 import org.sunbird.graph.utils.NodeUtil
+import org.sunbird.parseq.Task
 
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters.seqAsJavaListConverter
@@ -55,41 +56,41 @@ class ItemSetActor extends BaseActor {
 
 	def review(request: Request): Future[Response] = {
 		val identifier = request.getContext.get("identifier").asInstanceOf[String]
+		var flag = false
 		val readReq = new Request();
 		val reqContext = request.getContext
 		readReq.setContext(reqContext)
 		readReq.put("identifier", identifier)
 		readReq.put("fields", new util.ArrayList[String])
+		val updateReq = new Request()
+		updateReq.setContext(reqContext)
 		DataNode.read(readReq).map(node => {
 			if (CollectionUtils.isNotEmpty(node.getOutRelations)) {
 				//process relations with AssessmentItem
 				val itemRels: util.List[Relation] = node.getOutRelations.filter((rel: Relation) => StringUtils.equalsAnyIgnoreCase("AssessmentItem", rel.getEndNodeObjectType)).filterNot((reln: Relation) => StringUtils.equalsAnyIgnoreCase("Retired", reln.getEndNodeMetadata.get("status").toString))
 				val draftRelIds: List[String] = itemRels.filter((rel: Relation) => StringUtils.equalsAnyIgnoreCase("Draft", rel.getEndNodeMetadata.get("status").toString)).map(rel => rel.getEndNodeId).toList
 				if (CollectionUtils.isNotEmpty(draftRelIds)) {
-					val updateReq: Request = new Request();
-					updateReq.setContext(reqContext)
 					updateReq.put("identifiers", draftRelIds.asJava)
-					updateReq.put("metadata", new util.HashMap[String, AnyRef]() {
-						{
-							put("status", "Review")
-						}
-					})
-					DataNode.bulkUpdate(updateReq)
+					updateReq.put("metadata", new util.HashMap[String, AnyRef]() {{put("status", "Review")}})
+					flag = true
 				}
 				val newRels: util.List[util.HashMap[String, AnyRef]] = itemRels.sortBy((rel: Relation) => rel.getMetadata.get("IL_SEQUENCE_INDEX").asInstanceOf[Long])(Ordering.Long).map(rel => {
-					new util.HashMap[String, AnyRef]() {{
-							put("identifier", rel.getEndNodeId);
-						}}
-				}).toList
+					new util.HashMap[String, AnyRef]() {{put("identifier", rel.getEndNodeId);}}}).toList
 				request.put("items", newRels);
 			}
-			request.put("status","Review")
-			DataNode.update(request).map(node => {
-				val response: Response = ResponseHandler.OK
-				response.put("identifier", node.getIdentifier)
-				response
-			})
-		}).flatMap(f => f)
+			request.put("status", "Review")
+			val func = flag match {
+				case true => DataNode.bulkUpdate(updateReq).map(f => ResponseHandler.OK())
+				case false => Future(ResponseHandler.OK())
+			}
+			val futureList = Task.parallel[Response](func,
+				DataNode.update(request).map(node => {
+					val response: Response = ResponseHandler.OK
+					response.put("identifier", node.getIdentifier)
+					response
+				}))
+			futureList
+		}).flatMap(f => f).map(f => f.get(1))
 	}
 
 	def retire(request: Request): Future[Response] = {
