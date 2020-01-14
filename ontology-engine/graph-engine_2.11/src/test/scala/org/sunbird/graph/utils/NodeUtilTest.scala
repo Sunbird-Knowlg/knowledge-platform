f08c30e76af8d7e08bffcd200e26f9059c210995
Kumar Gauraw <er.krgauraw@gmail.com>
2020-01-14 12:37:03 +0530

    Issue #SB-17180 feat: added test cases for NodeUtil

--- ontology-engine/graph-engine_2.11/src/test/scala/org/sunbird/graph/utils/NodeUtilTest.scala
+++ ontology-engine/graph-engine_2.11/src/test/scala/org/sunbird/graph/utils/NodeUtilTest.scala
@@ -44,4 +44,27 @@ class NodeUtilTest extends FlatSpec with Matchers {
 		val result:util.List[String] = NodeUtil.getLanguageCodes(node)
 		result.isEmpty shouldBe true
 	}
+
+	"serialize with fields" should "return only node property which are present in fields" in {
+		val node: Node = new Node("do_1234", "DATA_NODE", "Content");
+		node.setMetadata(new util.HashMap[String, AnyRef]() {{
+				put("language", new util.ArrayList[String]() {{
+						add("English")
+					}})
+				put("contentType", "Resource")
+				put("name", "Test Resource Content")
+				put("code", "test.res.1")
+			}})
+		val fields: util.ArrayList[String] = new util.ArrayList[String]() {{
+				add("contentType")
+				add("name")
+			}}
+		val result: util.Map[String, AnyRef] = NodeUtil.serialize(node, fields, "content", "1.0")
+		result.isEmpty shouldBe false
+		result.size() shouldBe 4
+		result.containsKey("identifier") shouldBe true
+		result.containsKey("contentType") shouldBe true
+		result.containsKey("name") shouldBe true
+		result.containsKey("languageCode") shouldBe true
+	}
 }
