695efb61aedfb86b72b3f615643ab391fc4d610b
Kumar Gauraw <er.krgauraw@gmail.com>
2020-01-22 17:30:32 +0530

    Issue #SB-17163 fix: added test cases for NodeUtil

--- ontology-engine/graph-engine_2.11/src/test/scala/org/sunbird/graph/utils/NodeUtilTest.scala
+++ ontology-engine/graph-engine_2.11/src/test/scala/org/sunbird/graph/utils/NodeUtilTest.scala
@@ -2,6 +2,7 @@ package org.sunbird.graph.utils
 
 import java.util
 import org.scalatest.{FlatSpec, Matchers}
+import org.sunbird.common.JsonUtils
 import org.sunbird.graph.dac.model.Node
 
 class NodeUtilTest extends FlatSpec with Matchers {
@@ -27,5 +28,129 @@ class NodeUtilTest extends FlatSpec with Matchers {
 		result.containsKey("contentType") shouldBe true
 		result.containsKey("name") shouldBe true
 		result.containsKey("languageCode") shouldBe true
+	}
+
+	"deserialize with valid data" should "return a node with all data" in {
+		val nodeString :String = """{
+		                           |  "ownershipType": [
+		                           |    "createdBy"
+		                           |  ],
+		                           |  "code": "test.res.1",
+		                           |  "channel": "test",
+		                           |  "language": [
+		                           |    "English"
+		                           |  ],
+		                           |  "mimeType": "application/pdf",
+		                           |  "idealScreenSize": "normal",
+		                           |  "createdOn": "2020-01-17T16:17:39.931+0530",
+		                           |  "objectType": "Content",
+		                           |  "collections": [
+		                           |    {
+		                           |      "identifier": "LP_FT-74320",
+		                           |      "name": "LP_FT-74320",
+		                           |      "description": "test desc",
+		                           |      "objectType": "Content",
+		                           |      "relation": "hasSequenceMember",
+		                           |      "status": "Live"
+		                           |    }
+		                           |  ],
+		                           |  "contentDisposition": "inline",
+		                           |  "lastUpdatedOn": "2020-01-17T16:17:39.931+0530",
+		                           |  "contentEncoding": "identity",
+		                           |  "contentType": "Resource",
+		                           |  "dialcodeRequired": "No",
+		                           |  "identifier": "do_11293728197296947212",
+		                           |  "lastStatusChangedOn": "2020-01-17T16:17:39.931+0530",
+		                           |  "audience": [
+		                           |    "Learner"
+		                           |  ],
+		                           |  "os": [
+		                           |    "All"
+		                           |  ],
+		                           |  "visibility": "Default",
+		                           |  "resources": [
+		                           |    "Speaker",
+		                           |    "Touch"
+		                           |  ],
+		                           |  "mediaType": "content",
+		                           |  "osId": "org.ekstep.quiz.app",
+		                           |  "languageCode": [
+		                           |    "en"
+		                           |  ],
+		                           |  "version": 2,
+		                           |  "versionKey": "1579258059931",
+		                           |  "license": "CC BY 4.0",
+		                           |  "idealScreenDensity": "hdpi",
+		                           |  "framework": "NCF",
+		                           |  "concepts": [
+		                           |    {
+		                           |      "identifier": "LO1",
+		                           |      "name": "Word Meaning",
+		                           |      "description": "Understanding meaning of words",
+		                           |      "objectType": "Concept",
+		                           |      "relation": "associatedTo",
+		                           |      "status": "Live"
+		                           |    }
+		                           |  ],
+		                           |  "compatibilityLevel": 1,
+		                           |  "name": "Resource Content 1",
+		                           |  "status": "Live"
+		                           |}""".stripMargin
+		val nodeMap: util.Map[String, AnyRef] = JsonUtils.deserialize(nodeString.asInstanceOf[String], classOf[java.util.Map[String, AnyRef]])
+		val relString = """{
+		                  |  "concepts": {
+		                  |    "objects": [
+		                  |      "Concept"
+		                  |    ],
+		                  |    "type": "associatedTo",
+		                  |    "direction": "out"
+		                  |  },
+		                  |  "children": {
+		                  |    "objects": [
+		                  |      "Content",
+		                  |      "ContentImage"
+		                  |    ],
+		                  |    "type": "hasSequenceMember",
+		                  |    "direction": "out"
+		                  |  },
+		                  |  "collections": {
+		                  |    "objects": [
+		                  |      "Content",
+		                  |      "ContentImage"
+		                  |    ],
+		                  |    "type": "hasSequenceMember",
+		                  |    "direction": "in"
+		                  |  },
+		                  |  "usesContent": {
+		                  |    "objects": [
+		                  |      "Content"
+		                  |    ],
+		                  |    "type": "associatedTo",
+		                  |    "direction": "out"
+		                  |  },
+		                  |  "questions": {
+		                  |    "objects": [
+		                  |      "AssessmentItem"
+		                  |    ],
+		                  |    "type": "associatedTo",
+		                  |    "direction": "out"
+		                  |  },
+		                  |  "usedByContent": {
+		                  |    "objects": [
+		                  |      "Content"
+		                  |    ],
+		                  |    "type": "associatedTo",
+		                  |    "direction": "in"
+		                  |  }
+		                  |}""".stripMargin
+		val relationMap: util.Map[String, AnyRef] = JsonUtils.deserialize(relString.asInstanceOf[String], classOf[java.util.Map[String, AnyRef]])
+		val node = NodeUtil.deserialize(nodeMap, "content", relationMap)
+		node.getIdentifier shouldBe "do_11293728197296947212"
+		node.getOutRelations.size() shouldEqual 1
+		node.getOutRelations().get(0).getRelationType shouldEqual "associatedTo"
+		node.getOutRelations.get(0).getEndNodeId shouldEqual "LO1"
+		node.getInRelations.size() shouldEqual 1
+		node.getInRelations().get(0).getRelationType shouldEqual "hasSequenceMember"
+		node.getInRelations.get(0).getStartNodeId shouldEqual "LP_FT-74320"
 	}
 }
