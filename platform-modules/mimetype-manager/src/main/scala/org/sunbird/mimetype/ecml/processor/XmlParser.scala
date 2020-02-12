7b81351995a61623cda935b1f9ef24521c19db6b
Pradyumna Nagendra <pradyumnan@ilimi.in>
2020-02-12 14:29:09 +0530

    Issue #SB-15632 feat: ECML upload Implementation

---
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/ecml/processor/XmlParser.scala
@@ -0,0 +1,113 @@
+package org.sunbird.mimetype.ecml.processor
+
+import java.io.{StringReader, StringWriter}
+
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.{Transformer, TransformerFactory}
+import org.apache.commons.lang.StringUtils
+import org.sunbird.common.exception.ClientException
+import org.w3c.dom.{Element, Node}
+
+import scala.collection.mutable.ListBuffer
+import scala.xml._
+object XmlParser {
+
+    def parse(xml: String): Plugin = {
+        val strReader = new StringReader(xml)
+        try{
+            val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+            val document = builder.parse(new InputSource(strReader))
+            document.getDocumentElement.normalize()
+            val root = document.getDocumentElement
+            processDocument(root)
+        } finally {
+            if(null != strReader) strReader.close()
+
+        }
+    }
+
+    def processDocument(root: Element): Plugin = {
+        if(null != root) {
+            Plugin(getId(root), getData(root), "", getCdata(root), getChildrenPlugin(root), getManifest(root, true), getControllers(root), getEvents(root))
+        }else classOf[Plugin].newInstance()
+    }
+
+    def getAttributesMap(node: Node):Map[String, AnyRef] = {
+        (0 to node.getAttributes.getLength).toList.map(index => (node.getAttributes.item(index).getNodeName -> node.getAttributes.item(index).getNodeValue)).toMap
+    }
+
+    def getId(node: Node): String = {
+        getAttributesMap(node).getOrElse("id", "").asInstanceOf[String]
+    }
+
+    def getData(node: Node): Map[String, AnyRef] = {
+        if(null != node) Map("cwp_element_name" -> node.getNodeName) ++ getAttributesMap(node) else Map()
+    }
+
+    //TODO: Review the below code, this is as per the existing logic
+    def getCdata(node: Node): String = {
+        if(null != node && node.hasChildNodes){
+            val childNodes = node.getChildNodes
+            var cdata = ""
+            (0 to childNodes.getLength).toList.map(index => {
+                if(Node.CDATA_SECTION_NODE == childNodes.item(index).getNodeType)
+                    cdata = childNodes.item(index).getNodeValue
+            })
+            cdata
+        }else ""
+    }
+
+    def getChildrenPlugin(root: Element): List[Plugin] = {}
+
+    def getInnerText(manifestNode: Node): String = {}
+
+    def getNodeString(node: Node): String = {
+        val writer = new StringWriter()
+        try {
+            val transformer: Transformer = TransformerFactory.newInstance().newTransformer()
+            transformer.transform(new DOMSource(node), new StreamResult(writer))
+            val output: String = writer.toString
+            output.substring(output.indexOf("?>") + 2)
+        } finally {
+            writer.close()
+        }
+    }
+
+    def getMedia(node: Node, validateNode: Boolean): Media = {
+        if(null != node){
+            val attributeMap = getAttributesMap(node)
+            if(validateNode){
+                val id: String = attributeMap.getOrElse("id", "").asInstanceOf[String]
+                val `type`: String = attributeMap.getOrElse("type", "").asInstanceOf[String]
+                val src: String = attributeMap.getOrElse("src", "").asInstanceOf[String]
+                if(StringUtils.isBlank(id) && !(StringUtils.isNotBlank(`type`) && (StringUtils.equalsIgnoreCase(`type`, "js") || StringUtils.equalsIgnoreCase(`type`, "css"))))
+                    new ClientException("INVALID_MEDIA", "Error! Invalid Media ('id' is required.) in '" + getNodeString(node) + "' ...")
+            }
+        } else classOf[Media].newInstance()
+    }
+
+    def getManifest(node: Node, validateNode: Boolean): Manifest = {
+        val childNodes = node.getChildNodes
+        var manifestNode : Node = null
+        (0 to childNodes.getLength).toList.map(index => {
+            if(Node.CDATA_SECTION_NODE == childNodes.item(index).getNodeType)
+                manifestNode = childNodes.item(index)
+        })
+        val mediaList:ListBuffer[Media] = ListBuffer()
+        if(null != manifestNode && manifestNode.hasChildNodes) {
+            (0 to manifestNode.getChildNodes.getLength).toList.foreach(index => {
+                if(Node.ELEMENT_NODE == manifestNode.getChildNodes.item(index).getNodeType && "media".equalsIgnoreCase(manifestNode.getChildNodes.item(index).getNodeName))
+                    mediaList += getMedia(manifestNode.getChildNodes.item(index), validateNode)
+            })
+        }
+        Manifest(getId(manifestNode), getData(manifestNode), getInnerText(manifestNode), getCdata(manifestNode), mediaList.toList)
+    }
+
+    def getControllers(root: Element): List[Controller] = {}
+
+    def getEvents(root: Element): List[Event] = {}
+
+
+}
--- platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/mgr/impl/EcmlMimeTypeMgrImpl.scala
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/mgr/impl/EcmlMimeTypeMgrImpl.scala
@@ -2,14 +2,63 @@ package org.sunbird.mimetype.mgr.impl
 
 import java.io.File
 
+import org.sunbird.common.exception.ClientException
 import org.sunbird.graph.dac.model.Node
+import org.sunbird.mimetype.ecml.processor.Plugin
 import org.sunbird.mimetype.mgr.{BaseMimeTypeManager, MimeTypeManager}
+import org.sunbird.mimetype.util.FileUtils
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object EcmlMimeTypeMgrImpl extends BaseMimeTypeManager with MimeTypeManager {
-	override def upload(objectId: String, node: Node, uploadFile: File)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = ???
 
-	override def upload(objectId: String, node: Node, fileUrl: String)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = ???
+	def getEcmlType(basePath: String):String = {
+		val jsonFile = new File(basePath + File.separator + "index.json")
+		val xmlFile = new File(basePath + File.separator + "index.ecml")
+		if(null != jsonFile && jsonFile.exists() && null != xmlFile && xmlFile.exists()) throw new ClientException("MULTIPLE_ECML", "MULTIPLE_ECML_FILES_FOUND | [index.json and index.ecml]")
+		if(jsonFile.exists()) "json"
+		else if(xmlFile.exists()) "ecml"
+		else ""
+	}
+
+	override def upload(objectId: String, node: Node, uploadFile: File)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = {
+		FileUtils.validateFilePackage(uploadFile)
+		//generateECRF
+		val basePath:String = getBasePath(objectId)
+		FileUtils.extractPackage(uploadFile, basePath)
+
+		val ecmlType: String = getEcmlType(basePath)
+		val ecml = getFileString(basePath, ecmlType)
+		// generate ECML
+		val ecrf: Plugin = getEcrfObject(ecmlType, ecml);
+
+		//upload file
+		uploadArtifactToCloud(uploadFile, objectId)
+		//extractFile
+
+		Future{Map("artifactUrl" -> "http://ecmlartifact.zip")}
+	}
+
+	override def upload(objectId: String, node: Node, fileUrl: String)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = {
+		validateUploadRequest(objectId, node, fileUrl)
+		val file: File = copyURLToFile(objectId, fileUrl)
+		upload(objectId, node, file)
+	}
+
+
+	def getEcrfObject(ecmlType: String, ecml: String): Plugin = {
+		ecmlType match {
+			case "ecml" =>
+		}
+      null
+	}
+
+	def getFileString(basePath: String, ecmlType: String):String = {
+		ecmlType match {
+			case "ecml" => org.apache.commons.io.FileUtils.readFileToString(new File(basePath + File.separator + "index.ecml"), "UTF-8")
+			case "json" => org.apache.commons.io.FileUtils.readFileToString(new File(basePath + File.separator + "index.json"), "UTF-8")
+			case _ => ""
+		}
+	}
 
 }
