d21434d19c85dd3d3783bd5b65132d564851db52
Kumar Gauraw <er.krgauraw@gmail.com>
2020-02-28 15:28:54 +0530

    Issue #SC-0000 feat: updated html impl

--- platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/mgr/impl/HtmlMimeTypeMgrImpl.scala
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/mgr/impl/HtmlMimeTypeMgrImpl.scala
@@ -11,9 +11,14 @@ object HtmlMimeTypeMgrImpl extends BaseMimeTypeManager with MimeTypeManager {
 	override def upload(objectId: String, node: Node, uploadFile: File)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = {
 		validateUploadRequest(objectId, node, uploadFile)
 		isValidPackageStructure(uploadFile, List[String]("index.html"))
+		//TODO: Upload File to Cloud. Need Extraction utility for snapshot. slug should be false.
 		Future {Map[String, AnyRef]("identifier" -> objectId, "artifactUrl" -> "", "size" -> getFileSize(uploadFile).asInstanceOf[AnyRef])}
 	}
 
-	override def upload(objectId: String, node: Node, fileUrl: String)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = ???
+	override def upload(objectId: String, node: Node, fileUrl: String)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = {
+		validateUploadRequest(objectId, node, fileUrl)
+		val file = copyURLToFile(objectId, fileUrl);
+		upload(objectId, node, file)
+	}
 
 }
