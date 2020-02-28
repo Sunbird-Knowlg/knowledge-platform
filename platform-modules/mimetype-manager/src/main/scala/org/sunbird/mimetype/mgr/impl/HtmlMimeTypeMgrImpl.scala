42738f1ae9172d05238ea22c3ce71744ffa33660
Kumar Gauraw <er.krgauraw@gmail.com>
2020-02-28 15:28:24 +0530

    Issue#SB-15632 feat: added implementation for upload impls

--- platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/mgr/impl/HtmlMimeTypeMgrImpl.scala
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/mgr/impl/HtmlMimeTypeMgrImpl.scala
@@ -8,7 +8,11 @@ import org.sunbird.mimetype.mgr.{BaseMimeTypeManager, MimeTypeManager}
 import scala.concurrent.{ExecutionContext, Future}
 
 object HtmlMimeTypeMgrImpl extends BaseMimeTypeManager with MimeTypeManager {
-	override def upload(objectId: String, node: Node, uploadFile: File)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = ???
+	override def upload(objectId: String, node: Node, uploadFile: File)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = {
+		validateUploadRequest(objectId, node, uploadFile)
+		isValidPackageStructure(uploadFile, List[String]("index.html"))
+		Future {Map[String, AnyRef]("identifier" -> objectId, "artifactUrl" -> "", "size" -> getFileSize(uploadFile).asInstanceOf[AnyRef])}
+	}
 
 	override def upload(objectId: String, node: Node, fileUrl: String)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = ???
 
