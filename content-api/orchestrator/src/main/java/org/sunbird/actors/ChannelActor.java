d3526cf2ca6acc947dda4775ea37322412ac14fd
Umadevi <umadevi@Umadevis-MacBook-Pro.local>
2020-03-02 17:35:39 +0530

    implemented testcases for channel controller

--- content-api/orchestrator/src/main/java/org/sunbird/actors/ChannelActor.java
+++ content-api/orchestrator/src/main/java/org/sunbird/actors/ChannelActor.java
@@ -1,10 +1,28 @@
 package org.sunbird.actors;
 
+
+import akka.dispatch.Mapper;
+import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.core.BaseActor;
+import org.sunbird.cache.impl.RedisCache;
+import org.sunbird.common.ContentParams;
 import org.sunbird.common.dto.Request;
 import org.sunbird.common.dto.Response;
+import org.sunbird.common.dto.ResponseHandler;
+import org.sunbird.common.exception.ClientException;
+import org.sunbird.graph.dac.model.Node;
+import org.sunbird.graph.nodes.DataNode;
+import org.sunbird.graph.utils.NodeUtil;
+import org.sunbird.utils.RequestUtils;
 import scala.concurrent.Future;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.sunbird.common.Platform;
+import static java.util.stream.Collectors.toList;
 public class ChannelActor  {
 
 }
