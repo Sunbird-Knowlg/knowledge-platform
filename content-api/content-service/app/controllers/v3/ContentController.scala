a76979e8a71b2ecb6bd8c3eff496f32d95d86f57
AnjithaR <57887320+AnjithaR@users.noreply.github.com>
2020-03-03 15:02:09 +0530

    implemented framework controller apis in taxonomy service

--- content-api/content-service/app/controllers/v3/ContentController.scala
+++ content-api/content-service/app/controllers/v3/ContentController.scala
@@ -48,7 +48,6 @@ class ContentController @Inject()(@Named(ActorNames.CONTENT_ACTOR) contentActor:
         getResult(ApiId.READ_CONTENT, contentActor, readRequest)
     }
 
-
     def update(identifier: String) = Action.async { implicit request =>
         val headers = commonHeaders()
         val body = requestBody()
