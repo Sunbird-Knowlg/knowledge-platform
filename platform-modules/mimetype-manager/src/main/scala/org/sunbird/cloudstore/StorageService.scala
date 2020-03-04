62e3109a05ab53693653cd0a7c26283f2f6e9491
Mahesh Kumar Gangula <mahesh@ilimi.in>
2020-03-04 19:07:20 +0530

    Issue #000 fix: using getService method in StorageService.

--- platform-modules/mimetype-manager/src/main/scala/org/sunbird/cloudstore/StorageService.scala
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/cloudstore/StorageService.scala
@@ -15,7 +15,7 @@ import scala.concurrent.ExecutionContext
 trait StorageService {
 
     val storageType: String = Platform.config.getString("cloud_storage_type")
-    var storageService: BaseStorageService
+    var storageService: BaseStorageService = null
 
     @throws[Exception]
     def getService(): BaseStorageService = {
@@ -45,33 +45,33 @@ trait StorageService {
     def uploadFile(folderName: String, file: File, slug: Option[Boolean] = Option(true)): Array[String] = {
         val slugFile = if (slug.getOrElse(true)) Slug.createSlugFile(file) else file
         val objectKey = folderName + "/" + slugFile.getName
-        val url = storageService.upload(getContainerName, slugFile.getAbsolutePath, objectKey, Option.apply(false), Option.apply(1), Option.apply(5), Option.empty)
+        val url = getService.upload(getContainerName, slugFile.getAbsolutePath, objectKey, Option.apply(false), Option.apply(1), Option.apply(5), Option.empty)
         Array[String](objectKey, url)
     }
 
     def uploadDirectory(folderName: String, directory: File, slug: Option[Boolean] = Option(true)): Array[String] = {
         val slugFile = if (slug.getOrElse(true)) Slug.createSlugFile(directory) else directory
         val objectKey = folderName + File.separator
-        val url = storageService.upload(getContainerName(), slugFile.getAbsolutePath, objectKey, Option.apply(true), Option.apply(1), Option.apply(5), Option.empty)
+        val url = getService.upload(getContainerName(), slugFile.getAbsolutePath, objectKey, Option.apply(true), Option.apply(1), Option.apply(5), Option.empty)
         Array[String](objectKey, url)
     }
 
     def uploadDirectoryAsync(folderName: String, directory: File, slug: Option[Boolean] = Option(true))(implicit ec: ExecutionContext) = {
         val slugFile = if (slug.getOrElse(true)) Slug.createSlugFile(directory) else directory
         val objectKey = folderName + File.separator
-        storageService.uploadFolder(getContainerName, slugFile.getAbsolutePath, objectKey, Option.apply(false), None, None, 1)
+        getService.uploadFolder(getContainerName, slugFile.getAbsolutePath, objectKey, Option.apply(false), None, None, 1)
     }
 
     def getObjectSize(key: String): Double = {
-        val blob = storageService.getObject(getContainerName, key, Option.apply(false))
+        val blob = getService.getObject(getContainerName, key, Option.apply(false))
         blob.contentLength
     }
 
     def copyObjectsByPrefix(source: String, destination: String) = {
-        storageService.copyObjects(getContainerName, source, getContainerName, destination, Option.apply(true))
+        getService.copyObjects(getContainerName, source, getContainerName, destination, Option.apply(true))
     }
 
     def deleteFile(key: String, isDirectory: Option[Boolean] = Option(false)) = {
-        storageService.deleteObject(getContainerName, key, isDirectory)
+        getService.deleteObject(getContainerName, key, isDirectory)
     }
 }
