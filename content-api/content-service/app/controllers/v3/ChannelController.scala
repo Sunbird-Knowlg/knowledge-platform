69e88869ac3db6800fda05f4a91ade8a4b0793a0
Uma536 <55277404+Uma536@users.noreply.github.com>
2020-03-04 15:03:00 +0530

    implemented config.json files to objects

--- content-api/content-service/app/controllers/v3/ChannelController.scala
+++ content-api/content-service/app/controllers/v3/ChannelController.scala
@@ -47,5 +47,10 @@ class ChannelController  @Inject()(@Named(ActorNames.CONTENT_ACTOR) contentActor
     val response = JavaJsonUtils.serialize(result)
     Future(Ok(response).as("application/json"))
   }
+  def retire(identifier: String) = Action.async { implicit request =>
+    val result = ResponseHandler.OK()
+    val response = JavaJsonUtils.serialize(result)
+    Future(Ok(response).as("application/json"))
+  }
 }
 
