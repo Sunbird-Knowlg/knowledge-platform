888bc08930b9f9d60f51797a33d8dafaede680bc
Pradyumna Nagendra <pradyumnan@ilimi.in>
2020-03-06 16:54:51 +0530

    Issue #SB-15632 feat: Test cases for ecmlMimeType

--- platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/ecml/processor/EcrfObject.scala
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/ecml/processor/EcrfObject.scala
@@ -1,9 +1,21 @@
 package org.sunbird.mimetype.ecml.processor
 
-case class Plugin(id: String, data: Map[String, AnyRef], innerText: String, cData: String, childrenPlugin: List[Plugin], manifest: Manifest, controllers: List[Controller], events: List[Event])
-case class Manifest(id: String, data: Map[String, AnyRef], innerText: String, cData: String, medias: List[Media])
-case class Controller(id: String, data: Map[String, AnyRef], innerText: String, cData: String)
-case class Media(id: String, data: Map[String, AnyRef], innerText: String, cData: String, src: String, `type`: String, childrenPlugin: List[Plugin])
-case class Event(id: String, data: Map[String, AnyRef], innerText: String, cData: String, childrenPlugin: List[Plugin])
+import scala.annotation.Annotation
+
+case class Plugin(id: String, data: Map[String, AnyRef], innerText: String, cData: String, childrenPlugin: List[Plugin], manifest: Manifest, controllers: List[Controller], events: List[Event]) {
+    def this() = this("", null, "", "", null, null, null, null)
+}
+case class Manifest(id: String, data: Map[String, AnyRef], innerText: String, cData: String, medias: List[Media]) {
+    def this() = this("", null, "", "", null)
+}
+case class Controller(id: String, data: Map[String, AnyRef], innerText: String, cData: String) {
+    def this() = this("", null, "", "")
+}
+case class Media(id: String, data: Map[String, AnyRef], innerText: String, cData: String, src: String, `type`: String, childrenPlugin: List[Plugin]) {
+    def this() = this("", null, "", "", "", "", null)
+}
+case class Event(id: String, data: Map[String, AnyRef], innerText: String, cData: String, childrenPlugin: List[Plugin]) {
+    def this() = this("", null, "", "", null)
+}
 
 
