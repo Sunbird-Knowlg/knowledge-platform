0dfb9a04476f30982e74508f6ba6e6b2cfc02765
Pradyumna Nagendra <pradyumnan@ilimi.in>
2020-03-06 14:35:23 +0530

    Issue #SB-15632 feat: Test cases for mimeTypes

--- platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/mgr/impl/HtmlMimeTypeMgrImpl.scala
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/mgr/impl/HtmlMimeTypeMgrImpl.scala
@@ -11,17 +11,15 @@ import org.sunbird.telemetry.logger.TelemetryManager
 import scala.concurrent.{ExecutionContext, Future}
 
 class HtmlMimeTypeMgrImpl(implicit ss: StorageService) extends BaseMimeTypeManager with MimeTypeManager {
-    val mgr = new BaseMimeTypeManager()
 
     override def upload(objectId: String, node: Node, uploadFile: File)(implicit ec: ExecutionContext): Future[Map[String, AnyRef]] = {
         validateUploadRequest(objectId, node, uploadFile)
         if (isValidPackageStructure(uploadFile, List[String]("index.html"))) {
-            val urls = mgr.uploadArtifactToCloud(uploadFile, objectId)
+            val urls = uploadArtifactToCloud(uploadFile, objectId)
             node.getMetadata.put("s3Key", urls(IDX_S3_KEY))
             node.getMetadata.put("artifactUrl", urls(IDX_S3_URL))
-            mgr.extractPackageInCloud(objectId, uploadFile, node, "snapshot", false)
+            extractPackageInCloud(objectId, uploadFile, node, "snapshot", false)
             Future(Map[String, AnyRef]("identifier" -> objectId, "artifactUrl" -> urls(IDX_S3_URL), "s3Key" -> urls(IDX_S3_KEY), "size" -> getFileSize(uploadFile).asInstanceOf[AnyRef]))
-
         } else {
             TelemetryManager.error("ERR_INVALID_FILE" + "Please Provide Valid File! with file name: " + uploadFile.getName)
             throw new ClientException("ERR_INVALID_FILE", "Please Provide Valid File!")
