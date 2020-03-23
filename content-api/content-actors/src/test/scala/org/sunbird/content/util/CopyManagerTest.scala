a8bd97bc2df6b909cf3938c05efc9057ae42ed48
your username <rheafernandeshallu@gmail.com>
2020-03-23 17:22:54 +0530

    Issue #0000: Added copy content test case

--- content-api/content-actors/src/test/scala/org/sunbird/content/util/CopyManagerTest.scala
+++ content-api/content-actors/src/test/scala/org/sunbird/content/util/CopyManagerTest.scala
@@ -5,7 +5,8 @@ import java.util
 import org.scalamock.matchers.Matchers
 import org.scalamock.scalatest.{AsyncMockFactory, MockFactory}
 import org.scalatest.{AsyncFlatSpec, FlatSpec}
-import org.sunbird.common.dto.Request
+import org.sunbird.common.dto.{Property, Request}
+import org.sunbird.common.exception.ResponseCode
 import org.sunbird.graph.{GraphService, OntologyEngineContext}
 import org.sunbird.graph.dac.model.Node
 import org.sunbird.graph.service.operation.NodeAsyncOperations
@@ -16,13 +17,19 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class CopyManagerTest extends AsyncFlatSpec with Matchers with AsyncMockFactory {
 
-    "CopyManager" should "return copied node identifier when content is copied" ignore  {
+    "CopyManager" should "return copied node identifier when content is copied" in  {
         implicit val oec: OntologyEngineContext = mock[OntologyEngineContext]
         val graphDB = mock[GraphService]
-        (oec.graphService _).expects().returns(graphDB)
+        (oec.graphService _).expects().returns(graphDB).repeated(5)
         (graphDB.getNodeByUniqueId(_: String, _: String, _: Boolean, _: Request)).expects(*, *, *, *).returns(Future(getNode()))
+        (graphDB.addNode(_: String, _:Node)).expects(*, *).returns(Future(getCopiedNode()))
+        (graphDB.getNodeByUniqueId(_: String, _: String, _: Boolean, _: Request)).expects(*, *, *, *).returns(Future(getCopiedNode()))
+        (graphDB.upsertNode(_: String, _: Node, _: Request)).expects(*, *, *).returns(Future(getCopiedNode()))
+        (graphDB.getNodeProperty(_: String, _: String, _: String)).expects(*, *, *).returns(Future(new Property("versionKey", new org.neo4j.driver.internal.value.StringValue("1234"))))
         CopyManager.copy(getCopyRequest()).map(resp =>{
             assert(resp != null)
+            assert(resp.getResponseCode == ResponseCode.OK)
+            assert(resp.getResult.get("node_id").asInstanceOf[util.HashMap[String, AnyRef]].get("do_1234").asInstanceOf[String] == "do_1234_copy" )
         })
     }
 
@@ -33,6 +40,7 @@ class CopyManagerTest extends AsyncFlatSpec with Matchers with AsyncMockFactory 
     private def getNode(): Node = {
         val node = new Node()
         node.setIdentifier("do_1234")
+        node.setNodeType("DATA_NODE")
         node.setMetadata(new util.HashMap[String, AnyRef]() {
             {
                 put("identifier", "do_1234")
@@ -40,7 +48,30 @@ class CopyManagerTest extends AsyncFlatSpec with Matchers with AsyncMockFactory 
                 put("status", "Draft")
                 put("contentType", "Resource")
                 put("name", "Copy content")
-                put("artifactUrl", "https://sunbirddev.blob.core.windows.net/sunbird-content-dev/content/do_112980506913521664129/artifact/1584534572208_do_112980506913521664129.zip")
+                put("artifactUrl","https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/assets/do_212959046431154176151/hindi3.pdf")
+                put("channel", "in.ekstep")
+                put("code", "xyz")
+                put("versionKey", "1234")
+            }
+        })
+        node
+    }
+
+    private def getCopiedNode(): Node = {
+        val node = new Node()
+        node.setIdentifier("do_1234_copy")
+        node.setNodeType("DATA_NODE")
+        node.setMetadata(new util.HashMap[String, AnyRef]() {
+            {
+                put("identifier", "do_1234_copy")
+                put("mimeType", "application/pdf")
+                put("status", "Draft")
+                put("contentType", "Resource")
+                put("name", "Copy content")
+                put("artifactUrl", "https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/assets/do_212959046431154176151/hindi3.pdf")
+                put("channel", "in.ekstep")
+                put("code", "xyz")
+                put("versionKey", "1234")
             }
         })
         node
