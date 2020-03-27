c460460bab4d0d5958a618d62b1efdaf5b665b25
Mahesh Kumar Gangula <mahesh@ilimi.in>
2020-03-27 13:56:32 +0530

    Issue #000 fix: remove flink code.

--- platform-jobs/flink/flink-stream-sample/src/main/scala/org/sunbird/async/job/TestJobConfig.scala
+++
@@ -1,9 +0,0 @@
-package org.sunbird.async.job
-
-import org.sunbird.async.core.BaseJobConfig
-
-class TestJobConfig extends BaseJobConfig {
-
-    val kafkaInputTopic: String = config.getString("kafka.input.topic")
-    val kafkaSuccessTopic: String = config.getString("kafka.output.success.topic")
-}
