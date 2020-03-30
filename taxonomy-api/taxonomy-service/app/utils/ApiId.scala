66f4e225e562c219a53068edbac376801c3118b3
Uma536 <55277404+Uma536@users.noreply.github.com>
2020-03-30 14:54:05 +0530

    requested changes done in Apild.scala

--- taxonomy-api/taxonomy-service/app/utils/ApiId.scala
+++ taxonomy-api/taxonomy-service/app/utils/ApiId.scala
@@ -8,8 +8,8 @@ object ApiId {
 	final val UPDATE_FRAMEWORK = "api.framework.update"
 	final val RETIRE_FRAMEWORK = "api.framework.retire"
 	final val READ_FRAMEWORK = "api.framework.read"
-	final val UPDATE_FRAMEWORK_CATEGORY = "api.frameworkCategory.update"
-	final val  RETIRE_FRAMEWORK_CATEGORY = "api.frameworkCategory.retire"
+	final val UPDATE_FRAMEWORK_CATEGORY = "api.framework.category.update"
+	final val  RETIRE_FRAMEWORK_CATEGORY = "api.framework.category.retire"
 	final val CREATE_FRAMEWORK_CATEGORY = "api.framework.category.create"
 	final val READ_FRAMEWORK_CATEGORY = "api.framework.category.read"
 	
