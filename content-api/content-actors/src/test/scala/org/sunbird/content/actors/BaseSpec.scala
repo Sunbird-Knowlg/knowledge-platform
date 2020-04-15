59d19188e6a9c2d2cbf790935322683866234def
neha <neha0305verma@gmail.com>
2020-04-15 13:21:26 +0530

    removed embedded cassandra

--- content-api/content-actors/src/test/scala/org/sunbird/content/actors/BaseSpec.scala
+++ content-api/content-actors/src/test/scala/org/sunbird/content/actors/BaseSpec.scala
@@ -5,19 +5,16 @@ import java.util.concurrent.TimeUnit
 
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.TestKit
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+import org.scalatest.{FlatSpec, Matchers}
 import org.sunbird.common.dto.{Request, Response}
 import org.sunbird.graph.OntologyEngineContext
 import org.sunbird.graph.dac.model.Node
-import org.cassandraunit.utils.EmbeddedCassandraServerHelper
-import org.sunbird.cassandra.CassandraConnector
-import com.datastax.driver.core.Session
+
 import scala.concurrent.duration.FiniteDuration
 
-class BaseSpec extends FlatSpec with Matchers with BeforeAndAfterAll{
+class BaseSpec extends FlatSpec with Matchers {
 
     val system = ActorSystem.create("system")
-    var session: Session = null
 
     def testUnknownOperation(props: Props, request: Request)(implicit oec: OntologyEngineContext) = {
         request.setOperation("unknown")
@@ -42,23 +39,5 @@ class BaseSpec extends FlatSpec with Matchers with BeforeAndAfterAll{
         }})
         node.setMetadata(nodeMetadata)
         node
-    }
-
-    def setUpEmbeddedCassandra(): Unit = {
-        System.setProperty("cassandra.unsafesystem", "true")
-        EmbeddedCassandraServerHelper.startEmbeddedCassandra("/cassandra-unit.yaml", 100000L)
-    }
-
-    override def beforeAll(): Unit = {
-        setUpEmbeddedCassandra()
-    }
-
-    def executeCassandraQuery(queries: String*): Unit = {
-        if(null == session || session.isClosed){
-            session = CassandraConnector.getSession
-        }
-        for(query <- queries) {
-            session.execute(query)
-        }
     }
 }
--- content-api/content-actors/src/test/scala/org/sunbird/content/util/TestAcceptFlagManager.scala
+++ content-api/content-actors/src/test/scala/org/sunbird/content/util/TestAcceptFlagManager.scala
@@ -5,7 +5,7 @@ import java.util
 import akka.actor.Props
 import org.scalamock.scalatest.MockFactory
 import org.sunbird.cloudstore.StorageService
-import org.sunbird.common.dto.{Property, Request, ResponseHandler}
+import org.sunbird.common.dto.{Property, Request}
 import org.sunbird.content.actors.{BaseSpec, ContentActor}
 import org.sunbird.graph.{GraphService, OntologyEngineContext}
 import org.sunbird.graph.dac.model.Node
@@ -15,15 +15,6 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class TestAcceptFlagManager extends BaseSpec with MockFactory {
-
-  private val script_1 = "CREATE KEYSPACE IF NOT EXISTS hierarchy_store WITH replication = {'class': 'SimpleStrategy','replication_factor': '1'};"
-  private val script_2 = "CREATE TABLE IF NOT EXISTS hierarchy_store.content_hierarchy (identifier text, hierarchy text,PRIMARY KEY (identifier));"
-  private val script_3 = "INSERT INTO hierarchy_store.content_hierarchy(identifier, hierarchy) values ('domain', '{\"identifier\":\"domain\",\"children\":[{\"parent\":\"domain\",\"identifier\":\"domain\",\"copyright\":\"Sunbird\",\"lastStatusChangedOn\":\"2019-08-21T14:37:50.281+0000\",\"code\":\"2e837725-d663-45da-8ace-9577ab111982\",\"visibility\":\"Parent\",\"index\":1,\"mimeType\":\"application/vnd.ekstep.content-collection\",\"createdOn\":\"2019-08-21T14:37:50.281+0000\",\"versionKey\":\"1566398270281\",\"framework\":\"tpd\",\"depth\":1,\"children\":[],\"name\":\"U1\",\"lastUpdatedOn\":\"2019-08-21T14:37:50.281+0000\",\"contentType\":\"CourseUnit\",\"status\":\"Live\"}],\"status\":\"Live\"}');"
-
-  override def beforeAll(): Unit = {
-    super.beforeAll()
-    executeCassandraQuery(script_1, script_2, script_3)
-  }
 
   it should "return success response for acceptFlag for Resource" in {
     implicit val ss = mock[StorageService]
@@ -49,36 +40,6 @@ class TestAcceptFlagManager extends BaseSpec with MockFactory {
     request.getRequest.putAll(mapAsJavaMap(Map("identifier" -> "domain")))
     request.setOperation("acceptFlag")
     val response = callActor(request, Props(new ContentActor()))
-    assert("successful".equals(response.getParams.getStatus))
-  }
-
-  it should "return success response for acceptFlag for Collection" in {
-    implicit val ss = mock[StorageService]
-    implicit val oec: OntologyEngineContext = mock[OntologyEngineContext]
-    val graphDB = mock[GraphService]
-    (oec.graphService _).expects().returns(graphDB).anyNumberOfTimes()
-    val nodeMetaData = new util.HashMap[String, AnyRef]() {{
-      put("name", "Domain")
-      put("code", "domain")
-      put("status", "Flagged")
-      put("identifier", "domain")
-      put("versionKey", "1234")
-      put("contentType", "TextBook")
-      put("channel", "Test")
-      put("mimeType", "application/vnd.ekstep.content-collection")
-    }}
-    val node = getNode("Content", Option(nodeMetaData))
-    (graphDB.getNodeByUniqueId(_: String, _: String, _: Boolean, _: Request)).expects(*, *, *, *).returns(Future(node)).anyNumberOfTimes()
-    (graphDB.upsertNode(_:String, _: Node, _: Request)).expects(*, *, *).returns(Future(node)).anyNumberOfTimes()
-    (graphDB.getNodeProperty(_: String, _: String, _: String)).expects(*, *, *).returns(Future(new Property("versionKey", new org.neo4j.driver.internal.value.StringValue("1234")))).anyNumberOfTimes()
-    val resp = ResponseHandler.OK()
-    resp.getResult.put("content", new util.HashMap[String, AnyRef])
-    val request = getRequest()
-    request.getContext.put("identifier","domain")
-    request.getRequest.putAll(mapAsJavaMap(Map("identifier" -> "domain")))
-    request.setOperation("acceptFlag")
-    val response = callActor(request, Props(new ContentActor()))
-    println("response :"+response.getResponseCode)
     assert("successful".equals(response.getParams.getStatus))
   }
 
