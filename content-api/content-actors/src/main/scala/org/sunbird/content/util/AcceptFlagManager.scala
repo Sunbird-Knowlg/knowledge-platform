dbe990b4552ac142d04d28355b4308f7ad46932f
neha <neha0305verma@gmail.com>
2020-04-23 10:35:56 +0530

    removed updateHierarchy

--- content-api/content-actors/src/main/scala/org/sunbird/content/util/AcceptFlagManager.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/util/AcceptFlagManager.scala
@@ -4,12 +4,13 @@ import java.util
 
 import org.apache.commons.lang.StringUtils
 import org.sunbird.cache.impl.RedisCache
+import org.sunbird.common.{DateUtils, Platform}
 import org.sunbird.common.dto.{Request, Response, ResponseHandler}
 import org.sunbird.common.exception.ResponseCode
 import org.sunbird.graph.OntologyEngineContext
 import org.sunbird.graph.nodes.DataNode
 import org.sunbird.graph.dac.model.Node
-import org.sunbird.graph.external.ExternalPropsManager
+import org.sunbird.graph.service.common.DACConfigurationConstants
 import org.sunbird.graph.utils.ScalaJsonUtils
 import org.sunbird.managers.HierarchyManager
 
@@ -24,10 +25,11 @@ object AcceptFlagManager {
       } else {
         request.getContext.put(ContentConstants.IDENTIFIER, node.getIdentifier)
         createOrUpdateImageNode(request, node).map(imgNode => {
-          updateOriginalNode(request).map(response => {
+          updateOriginalNode(request, node).map(response => {
             if (!ResponseHandler.checkError(response)) {
-              response.getResult.put(ContentConstants.NODE_ID, node.getIdentifier)
-              response.getResult.put(ContentConstants.VERSION_KEY, imgNode.getMetadata.get(ContentConstants.VERSION_KEY))
+              response.put(ContentConstants.NODE_ID, node.getIdentifier)
+              response.put(ContentConstants.IDENTIFIER, node.getIdentifier)
+              response.put(ContentConstants.VERSION_KEY, imgNode.getMetadata.get(ContentConstants.VERSION_KEY))
               response
             } else {
               response
@@ -40,59 +42,49 @@ object AcceptFlagManager {
 
   protected def createOrUpdateImageNode(request: Request, node: Node)(implicit ec: ExecutionContext, oec: OntologyEngineContext): Future[Node] = {
     val req: Request = new Request(request)
-    req.getRequest.put(ContentConstants.STATUS, ContentConstants.FLAG_DRAFT)
-    req.getRequest.put(ContentConstants.IDENTIFIER, node.getIdentifier)
+    req.put(ContentConstants.STATUS, ContentConstants.FLAG_DRAFT)
+    req.put(ContentConstants.IDENTIFIER, node.getIdentifier)
     DataNode.update(req).map(node => {
       node
     })
   }
 
-  protected def updateOriginalNode(request: Request)(implicit ec: ExecutionContext, oec: OntologyEngineContext): Future[Response] = {
-    request.getRequest.put(ContentConstants.STATUS, ContentConstants.RETIRED)
+  protected def updateOriginalNode(request: Request, node: Node)(implicit ec: ExecutionContext, oec: OntologyEngineContext): Future[Response] = {
+    val currentDate = DateUtils.formatCurrentDate
+    request.put(ContentConstants.STATUS, ContentConstants.RETIRED)
+    request.put(ContentConstants.LAST_STATUS_CHANGED_ON, currentDate)
+    request.put(ContentConstants.LAST_UPDATED_ON, currentDate)
+    request.put(ContentConstants.VERSION_KEY, Platform.config.getString(DACConfigurationConstants.PASSPORT_KEY_BASE_PROPERTY))
+
     request.getContext.put(ContentConstants.VERSIONING, ContentConstants.DISABLE)
-    DataNode.update(request).map(updatedOriginalNode => {
-      if (StringUtils.equals(updatedOriginalNode.getMetadata.getOrDefault(ContentConstants.MIME_TYPE, "").asInstanceOf[String], ContentConstants.COLLECTION_MIME_TYPE)) {
-        request.put(ContentConstants.ROOT_ID, updatedOriginalNode.getIdentifier)
-        val updateMetadata = Map(
-          ContentConstants.VERSION_KEY -> updatedOriginalNode.getMetadata.get(ContentConstants.VERSION_KEY).asInstanceOf[String],
-          ContentConstants.LAST_STATUS_CHANGED_ON -> updatedOriginalNode.getMetadata.get(ContentConstants.LAST_STATUS_CHANGED_ON).asInstanceOf[String],
-          ContentConstants.LAST_UPDATED_ON -> updatedOriginalNode.getMetadata.get(ContentConstants.LAST_UPDATED_ON).asInstanceOf[String]
-        )
-        updateHierarchy(request, updateMetadata).map(hierarchyResponse => {
-          if (!ResponseHandler.checkError(hierarchyResponse)) {
+
+    if (StringUtils.equals(node.getMetadata.getOrDefault(ContentConstants.MIME_TYPE, "").asInstanceOf[String], ContentConstants.COLLECTION_MIME_TYPE)) {
+      request.getContext.put(ContentConstants.SCHEMA_NAME, ContentConstants.COLLECTION_SCHEMA_NAME)
+      request.put(ContentConstants.ROOT_ID, request.get(ContentConstants.IDENTIFIER))
+      HierarchyManager.getHierarchy(request).map(hierarchyResponse => {
+        if (!ResponseHandler.checkError(hierarchyResponse)) {
+          val updatedHierarchy = hierarchyResponse.get(ContentConstants.CONTENT).asInstanceOf[util.Map[String, AnyRef]]
+          updatedHierarchy.putAll(new util.HashMap[String, AnyRef]() {
+            {
+              put(ContentConstants.STATUS, ContentConstants.RETIRED)
+              put(ContentConstants.LAST_STATUS_CHANGED_ON, currentDate)
+              put(ContentConstants.LAST_UPDATED_ON, currentDate)
+            }
+          })
+          request.put(ContentConstants.HIERARCHY, ScalaJsonUtils.serialize(updatedHierarchy))
+          DataNode.update(request).map(updatedOriginalNode => {
+            RedisCache.delete(ContentConstants.HIERARCHY_PREFIX + request.get(ContentConstants.IDENTIFIER))
             ResponseHandler.OK()
-          } else {
-            hierarchyResponse
-          }
-        })
-      } else {
-        Future(ResponseHandler.OK())
-      }
-    }).flatMap(f => f)
+          })
+        } else {
+          Future(hierarchyResponse)
+        }
+      }).flatMap(f => f)
+    } else {
+      DataNode.update(request).map(updatedOriginalNode => {
+        ResponseHandler.OK()
+      })
+    }
   }
 
-  protected def updateHierarchy(request: Request, updateMetadata: Map[String, AnyRef])(implicit ec: ExecutionContext, oec: OntologyEngineContext): Future[Response] = {
-    request.getContext.put(ContentConstants.SCHEMA_NAME, ContentConstants.COLLECTION_SCHEMA_NAME)
-    HierarchyManager.getHierarchy(request).map(hierarchyResponse => {
-      if (!ResponseHandler.checkError(hierarchyResponse)) {
-        val updatedHierarchy = hierarchyResponse.getResult.get(ContentConstants.CONTENT).asInstanceOf[util.Map[String, AnyRef]]
-        updatedHierarchy.putAll(new util.HashMap[String, AnyRef]() {
-          {
-            put(ContentConstants.STATUS, ContentConstants.RETIRED)
-            put(ContentConstants.VERSION_KEY, updateMetadata.get(ContentConstants.VERSION_KEY))
-            put(ContentConstants.LAST_STATUS_CHANGED_ON, updateMetadata.get(ContentConstants.LAST_STATUS_CHANGED_ON))
-            put(ContentConstants.LAST_UPDATED_ON, updateMetadata.get(ContentConstants.LAST_UPDATED_ON))
-          }
-        })
-        val req: Request = new Request(request)
-        req.put(ContentConstants.HIERARCHY, ScalaJsonUtils.serialize(updatedHierarchy))
-        req.put(ContentConstants.IDENTIFIER, request.get(ContentConstants.ROOT_ID))
-        ExternalPropsManager.saveProps(req)
-        RedisCache.delete(ContentConstants.HIERARCHY_PREFIX + request.get(ContentConstants.ROOT_ID))
-        ResponseHandler.OK
-      } else {
-        hierarchyResponse
-      }
-    })
-  }
 }
