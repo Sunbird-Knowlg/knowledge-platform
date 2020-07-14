1d39ccea4b30fd66dcd056995297635a54f02f66
rheafernandes <rheafernandeshallu@gmail.com>
2020-07-14 15:05:02 +0530

    Added Testcases for ecml and content spec

--- content-api/content-service/test/controllers/v3/ContentSpec.scala
+++ content-api/content-service/test/controllers/v3/ContentSpec.scala
@@ -162,6 +162,7 @@ class ContentSpec extends BaseSpec {
         isOK(result)
         status(result) must equalTo(OK)
     }
+
     "return success response for upload API with fileUrl" in {
         val controller = app.injector.instanceOf[controllers.v3.ContentController]
         val file = new File("test/resources/sample.pdf")
@@ -169,6 +170,17 @@ class ContentSpec extends BaseSpec {
         val multipartBody = MultipartFormData(Map[String, Seq[String]]("fileUrl" -> Seq("https://abc.com/content/sample.pdf"), "filePath" -> Seq("/program/id")), files, Seq[BadPart]())
         val fakeRequest = FakeRequest().withMultipartFormDataBody(multipartBody)
         val result = controller.upload("01234", UploadParams())(fakeRequest)
+        isOK(result)
+        status(result) must equalTo(OK)
+    }
+
+    "return success response for upload API with fileUrl and fileFormat" in {
+        val controller = app.injector.instanceOf[controllers.v3.ContentController]
+        val file = new File("test/resources/sample.pdf")
+        val files = Seq[FilePart[TemporaryFile]](FilePart("file", "sample.pdf", None, SingletonTemporaryFileCreator.create(file.toPath)))
+        val multipartBody = MultipartFormData(Map[String, Seq[String]](), files, Seq[BadPart]())
+        val fakeRequest = FakeRequest().withMultipartFormDataBody(multipartBody)
+        val result = controller.upload("01234", UploadParams("composed-h5p-zip"))(fakeRequest)
         isOK(result)
         status(result) must equalTo(OK)
     }
--- platform-modules/mimetype-manager/src/test/scala/org/sunbird/mimetype/mgr/impl/EcmlMimeTypeMgrImplTest.scala
+++ platform-modules/mimetype-manager/src/test/scala/org/sunbird/mimetype/mgr/impl/EcmlMimeTypeMgrImplTest.scala
@@ -64,6 +64,22 @@ class EcmlMimeTypeMgrImplTest extends AsyncFlatSpec with Matchers with AsyncMock
         assert(true)
     }
 
+    it should "upload ECML with json zip file URL and return public url" in {
+        val node = getNode()
+        val identifier = "do_1234"
+        implicit val ss = mock[StorageService]
+        (ss.uploadFile(_:String, _: File, _: Option[Boolean])).expects(*, *, *).returns(Array(identifier, identifier))
+        (ss.uploadDirectory(_:String, _:File, _: Option[Boolean])).expects(*, *, *)
+        val resFuture = new EcmlMimeTypeMgrImpl().upload(identifier, node, "https://sunbirddev.blob.core.windows.net/sunbird-content-dev/content/kp_ft_1594670084387/artifact/ecml_with_json.zip", None, UploadParams())
+        resFuture.map(result => {
+            assert(null != result)
+            assert(result.nonEmpty)
+            assert("do_123" == result.getOrElse("identifier",""))
+        })
+
+        assert(true)
+    }
+
     def getNode(): Node = {
         val node = new Node()
         node.setIdentifier("do_1234")
