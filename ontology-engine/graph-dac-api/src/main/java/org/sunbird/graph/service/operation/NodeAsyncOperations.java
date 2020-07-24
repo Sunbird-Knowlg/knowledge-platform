101b838139796eb8ed2b8ff4677a7b74a592befd
rheafernandes <rheafernandeshallu@gmail.com>
2020-07-24 11:01:30 +0530

    Removed extra code changes

--- ontology-engine/graph-dac-api/src/main/java/org/sunbird/graph/service/operation/NodeAsyncOperations.java
+++ ontology-engine/graph-dac-api/src/main/java/org/sunbird/graph/service/operation/NodeAsyncOperations.java
@@ -43,7 +43,7 @@ public class NodeAsyncOperations {
     private final static String DEFAULT_CYPHER_NODE_OBJECT = "ee";
 
 
-    public static Future<Node> addNode(String graphId, Node node) throws Exception {
+    public static Future<Node> addNode(String graphId, Node node) {
         if (StringUtils.isBlank(graphId))
             throw new ClientException(DACErrorCodeConstants.INVALID_GRAPH.name(),
                     DACErrorMessageConstants.INVALID_GRAPH_ID + " | [Create Node Operation Failed.]");
@@ -97,7 +97,7 @@ public class NodeAsyncOperations {
         }
     }
 
-    public static Future<Node> upsertNode(String graphId, Node node, Request request) throws Exception {
+    public static Future<Node> upsertNode(String graphId, Node node, Request request) {
         TelemetryManager.log("Applying the Consumer Authorization Check for Node Id: " + node.getIdentifier());
         setRequestContextToNode(node, request);
         validateAuthorization(graphId, node, request);
