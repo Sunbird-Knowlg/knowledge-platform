9a984a61cab6c66f99bebf65fd0d3fe14a336226
NehaVerma <neha0305verma@gmail.com>
2020-08-28 11:24:07 +0530

    SC-1873 : testcase fix

--- ontology-engine/graph-engine_2.11/src/test/scala/org/sunbird/graph/nodes/TestDataNode.scala
+++ ontology-engine/graph-engine_2.11/src/test/scala/org/sunbird/graph/nodes/TestDataNode.scala
@@ -288,6 +288,7 @@ class TestDataNode extends BaseSpec {
         request.getContext.put("identifier", "do_1129067102240194561252")
         request.put("name", "updated name")
         request.put("versionKey", "1575527498230")
+        request.put("primaryCategory", "LearningResource")
         val updateFuture = DataNode.update(request)
         updateFuture.map(node => {
             assert(node.getIdentifier.equalsIgnoreCase("do_1129067102240194561252.img"))
