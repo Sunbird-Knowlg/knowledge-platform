2c962b39d5f7d6c9341a369aa37d98ccc0131f43
Kumar Gauraw <er.krgauraw@gmail.com>
2020-09-08 00:22:32 +0530

    Issue #SC-1987 feat: added object category definition api

---
+++ taxonomy-api/taxonomy-actors/src/main/scala/org/sunbird/actors/ObjectCategoryDefinitionActor.scala
@@ -0,0 +1,78 @@
+package org.sunbird.actors
+
+import java.util
+
+import javax.inject.Inject
+import org.apache.commons.lang3.StringUtils
+import org.sunbird.actor.core.BaseActor
+import org.sunbird.common.Slug
+import org.sunbird.common.dto.{Request, Response, ResponseHandler}
+import org.sunbird.common.exception.ClientException
+import org.sunbird.graph.OntologyEngineContext
+import org.sunbird.graph.nodes.DataNode
+import org.sunbird.graph.utils.NodeUtil
+import org.sunbird.utils.{Constants, RequestUtil}
+
+import scala.collection.JavaConverters
+import scala.concurrent.{ExecutionContext, Future}
+
+class ObjectCategoryDefinitionActor @Inject()(implicit oec: OntologyEngineContext) extends BaseActor {
+
+	implicit val ec: ExecutionContext = getContext().dispatcher
+
+	override def onReceive(request: Request): Future[Response] = {
+		request.getOperation match {
+			case Constants.CREATE_OBJECT_CATEGORY_DEFINITION => create(request)
+			case Constants.READ_OBJECT_CATEGORY_DEFINITION => read(request)
+			case Constants.UPDATE_OBJECT_CATEGORY_DEFINITION => update(request)
+			case _ => ERROR(request.getOperation)
+		}
+	}
+
+	private def create(request: Request): Future[Response] = {
+		RequestUtil.restrictProperties(request)
+		val categoryId = request.getRequest.getOrDefault(Constants.CATEGORY_ID, "").asInstanceOf[String]
+		val channelId = request.getRequest.getOrDefault(Constants.CHANNEL, "all").asInstanceOf[String]
+		val targetObjectType = request.getRequest.getOrDefault(Constants.TARGET_OBJECT_TYPE, "").asInstanceOf[String]
+		if (StringUtils.isNotBlank(categoryId)) {
+			val identifier = Slug.makeSlug(categoryId) + "_" + Slug.makeSlug(targetObjectType) + "_" + channelId
+			request.put(Constants.IDENTIFIER, identifier)
+			val getCategoryReq = new Request()
+			getCategoryReq.setContext(request.getContext)
+			getCategoryReq.getContext.put(Constants.SCHEMA_NAME, Constants.OBJECT_CATEGORY_SCHEMA_NAME)
+			getCategoryReq.getContext.put(Constants.VERSION, Constants.OBJECT_CATEGORY_SCHEMA_VERSION)
+			getCategoryReq.put(Constants.IDENTIFIER, categoryId)
+			getCategoryReq.put("fields", new util.ArrayList[String])
+			DataNode.read(getCategoryReq).map(node => {
+				if (null != node && StringUtils.equalsAnyIgnoreCase(node.getIdentifier, categoryId)) {
+					DataNode.create(request).map(node => {
+						val response = ResponseHandler.OK
+						response.put(Constants.IDENTIFIER, node.getIdentifier)
+						response
+					})
+				} else throw new ClientException("ERR_INVALID_CATEGORY_ID", "Please provide valid category identifier")
+			}).flatMap(f => f)
+		} else throw new ClientException("ERR_INVALID_CATEGORY_ID", "Please provide valid category identifier")
+	}
+
+	private def read(request: Request): Future[Response] = {
+		val fields: util.List[String] = JavaConverters.seqAsJavaListConverter(request.get(Constants.FIELDS).asInstanceOf[String].split(",").filter(field => StringUtils.isNotBlank(field) && !StringUtils.equalsIgnoreCase(field, "null"))).asJava
+		request.getRequest.put(Constants.FIELDS, fields)
+		DataNode.read(request).map(node => {
+			val metadata: util.Map[String, AnyRef] = NodeUtil.serialize(node, fields, request.getContext.get(Constants.SCHEMA_NAME).asInstanceOf[String], request.getContext.get(Constants.VERSION).asInstanceOf[String])
+			val response: Response = ResponseHandler.OK
+			response.put(Constants.OBJECT_CATEGORY_DEFINITION, metadata)
+			response
+		})
+	}
+
+	private def update(request: Request): Future[Response] = {
+		RequestUtil.restrictProperties(request)
+		DataNode.update(request).map(node => {
+			val response: Response = ResponseHandler.OK
+			response.put(Constants.IDENTIFIER, node.getIdentifier)
+			response
+		})
+	}
+
+}
---
+++ taxonomy-api/taxonomy-actors/src/test/scala/org/sunbird/actors/ObjectCategoryDefinitionActorTest.scala
@@ -0,0 +1,174 @@
+package org.sunbird.actors
+
+import java.util
+
+import akka.actor.Props
+import org.apache.commons.lang3.StringUtils
+import org.scalamock.scalatest.MockFactory
+import org.sunbird.common.dto.Request
+import org.sunbird.common.exception.ResponseCode
+import org.sunbird.graph.{GraphService, OntologyEngineContext}
+import org.sunbird.graph.dac.model.Node
+import org.sunbird.utils.Constants
+
+import scala.collection.JavaConversions.mapAsJavaMap
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class ObjectCategoryDefinitionActorTest extends BaseSpec with MockFactory {
+
+	"ObjectCategoryDefinitionActor" should "return failed response for 'unknown' operation" in {
+		implicit val oec: OntologyEngineContext = new OntologyEngineContext
+		testUnknownOperation(Props(new ObjectCategoryDefinitionActor()), getCategoryDefintionRequest())
+	}
+
+	it should "create a categoryDefinitionNode and store it in neo4j" in {
+		implicit val oec: OntologyEngineContext = mock[OntologyEngineContext]
+		val graphDB = mock[GraphService]
+		(oec.graphService _).expects().returns(graphDB).anyNumberOfTimes()
+		val node = new Node()
+		node.setIdentifier("obj-cat:1234")
+		node.setMetadata(new util.HashMap[String, AnyRef]() {
+			{
+				put("identifier", "obj-cat:1234");
+				put("objectType", "ObjectCategory")
+			}
+		})
+		(graphDB.getNodeByUniqueId(_: String, _: String, _: Boolean, _: Request)).expects(*, *, *, *).returns(Future(node))
+		(graphDB.addNode(_: String, _: Node)).expects(*, *).returns(Future(getCategoryDefinitionNode()))
+		val request = getCategoryDefintionRequest()
+		request.putAll(mapAsJavaMap(Map("name" -> "Test Category Definition", "targetObjectType" -> "Content", "categoryId" -> "obj-cat:1234", "objectMetadata" -> Map("schema" -> Map()), "config" -> Map())))
+		request.setOperation(Constants.CREATE_OBJECT_CATEGORY_DEFINITION)
+		val response = callActor(request, Props(new ObjectCategoryDefinitionActor()))
+		assert(response.get(Constants.IDENTIFIER) != null)
+		assert(response.get(Constants.IDENTIFIER).equals("obj-cat:1234_content_all"))
+	}
+
+	it should "should throw exception if get category node returns null" in {
+		implicit val oec: OntologyEngineContext = mock[OntologyEngineContext]
+		val graphDB = mock[GraphService]
+		(oec.graphService _).expects().returns(graphDB).anyNumberOfTimes()
+		(graphDB.getNodeByUniqueId(_: String, _: String, _: Boolean, _: Request)).expects(*, *, *, *).returns(Future(null))
+		val request = getCategoryDefintionRequest()
+		request.putAll(mapAsJavaMap(Map("name" -> "Test Category Definition", "targetObjectType" -> "Content", "categoryId" -> "obj-cat:1234", "objectMetadata" -> Map("schema" -> Map()), "config" -> Map())))
+		request.setOperation(Constants.CREATE_OBJECT_CATEGORY_DEFINITION)
+		val response = callActor(request, Props(new ObjectCategoryDefinitionActor()))
+		assert(response.getResponseCode == ResponseCode.CLIENT_ERROR)
+		assert(StringUtils.equalsIgnoreCase(response.getParams.getErrmsg, "Please provide valid category identifier"))
+	}
+
+	it should "should throw exception for blank categoryId" in {
+		implicit val oec: OntologyEngineContext = mock[OntologyEngineContext]
+		val request = getCategoryDefintionRequest()
+		request.putAll(mapAsJavaMap(Map("name" -> "Test Category Definition", "tagetObjectType" -> "Content", "categoryId" -> "")))
+		request.setOperation(Constants.CREATE_OBJECT_CATEGORY_DEFINITION)
+		val response = callActor(request, Props(new ObjectCategoryDefinitionActor()))
+		assert(response.getResponseCode == ResponseCode.CLIENT_ERROR)
+		assert(StringUtils.equalsIgnoreCase(response.getParams.getErrmsg, "Please provide valid category identifier"))
+	}
+
+	it should "return success response for readCategoryDefinition" in {
+		implicit val oec: OntologyEngineContext = mock[OntologyEngineContext]
+		val graphDB = mock[GraphService]
+		(oec.graphService _).expects().returns(graphDB).anyNumberOfTimes()
+		val node = getCategoryDefinitionNode()
+		(graphDB.getNodeByUniqueId(_: String, _: String, _: Boolean, _: Request)).expects(*, *, *, *).returns(Future(node)).anyNumberOfTimes()
+		val request = getCategoryDefintionRequest()
+		request.getContext.put(Constants.IDENTIFIER, "obj-cat:1234_content_all")
+		request.putAll(mapAsJavaMap(Map("fields" -> "")))
+		request.setOperation(Constants.READ_OBJECT_CATEGORY_DEFINITION)
+		val response = callActor(request, Props(new ObjectCategoryDefinitionActor()))
+		val objectCategoryDefinition = response.getResult.getOrDefault("objectCategoryDefinition", new util.HashMap[String, AnyRef]()).asInstanceOf[util.Map[String, AnyRef]]
+		assert("successful".equals(response.getParams.getStatus))
+		assert("obj-cat:1234_content_all".equals(objectCategoryDefinition.getOrDefault("identifier", "")))
+	}
+
+	it should "return success response for updateCategoryDefinition for valid input" in {
+		implicit val oec: OntologyEngineContext = mock[OntologyEngineContext]
+		val graphDB = mock[GraphService]
+		(oec.graphService _).expects().returns(graphDB).anyNumberOfTimes()
+		val node = new Node()
+		node.setIdentifier("obj-cat:1234_content_all")
+		node.setMetadata(new util.HashMap[String, AnyRef]() {
+			{
+				put("identifier", "obj-cat:1234_content_all")
+				put("categoryId", "obj-cat:1234")
+				put("objectType", "ObjectCategoryDefinition")
+				put("name", "Test Category Definition")
+				put("targetObjectType", "Content")
+				put("objectMetadata", "{\"schema\":{},\"config\":{}}")
+			}
+		})
+		(graphDB.getNodeByUniqueId(_: String, _: String, _: Boolean, _: Request)).expects(*, *, *, *).returns(Future(node)).anyNumberOfTimes()
+		(graphDB.upsertNode(_: String, _: Node, _: Request)).expects(*, *, *).returns(Future(getCategoryDefinitionNode()))
+		val request = getCategoryDefintionRequest()
+		request.getContext.put(Constants.IDENTIFIER, "obj-cat:1234_content_all")
+		request.putAll(mapAsJavaMap(Map("description" -> "test desc")))
+		request.setOperation(Constants.UPDATE_OBJECT_CATEGORY_DEFINITION)
+		val response = callActor(request, Props(new ObjectCategoryDefinitionActor()))
+		assert("successful".equals(response.getParams.getStatus))
+	}
+
+	it should "return client exception response for updateCategoryDefinition for invalid input" in {
+		implicit val oec: OntologyEngineContext = mock[OntologyEngineContext]
+		val graphDB = mock[GraphService]
+		(oec.graphService _).expects().returns(graphDB).anyNumberOfTimes()
+		val node = new Node()
+		node.setIdentifier("obj-cat:1234_content_all")
+		node.setMetadata(new util.HashMap[String, AnyRef]() {
+			{
+				put("identifier", "obj-cat:1234_content_all")
+				put("categoryId", "obj-cat:1234")
+				put("objectType", "ObjectCategoryDefinition")
+				put("name", "Test Category Definition")
+				put("targetObjectType", "Content")
+				put("objectMetadata", "{\"schema\":{},\"config\":{}}")
+			}
+		})
+		val request = getCategoryDefintionRequest()
+		request.getContext.put(Constants.IDENTIFIER, "obj-cat:1234_content_all")
+		request.putAll(mapAsJavaMap(Map("description" -> "test desc", "categoryId" -> "obj-cat:test-1234", "channel" -> "abc")))
+		request.setOperation(Constants.UPDATE_OBJECT_CATEGORY_DEFINITION)
+		val response = callActor(request, Props(new ObjectCategoryDefinitionActor()))
+		assert(response.getResponseCode == ResponseCode.CLIENT_ERROR)
+		assert(StringUtils.equalsIgnoreCase(response.getParams.getErrmsg, "Properties in list [identifier, categoryId, targetObjectType, channel, status, objectType] are not allowed in request"))
+	}
+
+	private def getCategoryDefintionRequest(): Request = {
+		val request = new Request()
+		request.setContext(getContext())
+		request
+	}
+
+	private def getContext(): util.Map[String, AnyRef] = new util.HashMap[String, AnyRef]() {
+		{
+			put("graph_id", "domain")
+			put("version", "1.0")
+			put("objectType", "ObjectCategoryDefinition")
+			put("schemaName", "objectcategorydefinition")
+
+		}
+	}
+
+	private def getCategoryDefinitionNode(): Node = {
+		val node = new Node()
+		node.setIdentifier("obj-cat:1234_content_all")
+		node.setNodeType("DATA_NODE")
+		node.setMetadata(new util.HashMap[String, AnyRef]() {
+			{
+				put("identifier", "obj-cat:1234_content_all")
+				put("categoryId", "obj-cat:1234")
+				put("objectType", "ObjectCategoryDefinition")
+				put("name", "Test Category Definition")
+				put("targetObjectType", "Content")
+				put("objectMetadata", new util.HashMap[String, AnyRef]() {
+					{
+						put("config", new util.HashMap())
+						put("schema", new util.HashMap())
+					}
+				})
+			}
+		})
+		node
+	}
+}
