068e398ef48ece4908a3b7b107297e6007d6911c
NehaVerma <neha0305verma@gmail.com>
2020-09-10 15:39:05 +0530

    SC-1955 : graph dac api Filter

--- ontology-engine/graph-dac-api/src/test/java/org/sunbird/graph/dac/model/FilterTest.java
+++ ontology-engine/graph-dac-api/src/test/java/org/sunbird/graph/dac/model/FilterTest.java
@@ -8,6 +8,12 @@ public class FilterTest {
     public static Filter filter_1 = null;
     public static Filter filter_2 = null;
     public static Filter filter_3 = null;
+    public static SearchCriteria sc = new SearchCriteria() {{
+        setGraphId("domain");
+        setNodeType("DATA_NODE");
+        setObjectType("Content");
+    }};
+    public static String param = "";
 
     @BeforeClass
     public static void init() {
@@ -18,6 +24,12 @@ public class FilterTest {
 
     @Test
     public void testFilterModel_1() {
+        filter_1.setOperator("");
+        Assert.assertEquals("=", filter_2.getOperator());
+    }
+
+    @Test
+    public void testFilterModel_2() {
         filter_1.setProperty("testProperty");
         filter_1.setValue("testValue");
         filter_1.setOperator("!=");
@@ -27,14 +39,83 @@ public class FilterTest {
     }
 
     @Test
-    public void testFilterModel_2() {
-        SearchCriteria sc = new SearchCriteria() {{
-           setGraphId("domain");
-           setNodeType("DATA_NODE");
-           setObjectType("Content");
-        }};
-        String param = "";
-        filter_1.getCypher(sc, param);
+    public void testFilterModel_3() {
+        filter_1.setProperty("identifier");
+        filter_1.setOperator("=");
+        String query = filter_1.getCypher(sc, param);
+        Assert.assertEquals(" n.IL_UNIQUE_ID = {4} ", query);
+    }
+
+    @Test
+    public void testFilterModel_4() {
+        filter_1.setProperty("identifier");
+        filter_1.setOperator("like");
+        String query = filter_1.getCypher(sc, param);
+        Assert.assertEquals(" n.IL_UNIQUE_ID =~ {5} ", query);
+    }
+
+    @Test
+    public void testFilterModel_5() {
+        filter_1.setProperty("identifier");
+        filter_1.setOperator("startsWith");
+        String query = filter_1.getCypher(sc, param);
+        Assert.assertEquals(" n.IL_UNIQUE_ID =~ {6} ", query);
+    }
+
+    @Test
+    public void testFilterModel_6() {
+        filter_1.setProperty("identifier");
+        filter_1.setOperator("endsWith");
+        String query = filter_1.getCypher(sc, param);
+        Assert.assertEquals(" n.IL_UNIQUE_ID =~ {7} ", query);
+    }
+
+    @Test
+    public void testFilterModel_7() {
+        filter_1.setProperty("identifier");
+        filter_1.setOperator(">");
+        String query = filter_1.getCypher(sc, param);
+        Assert.assertEquals(" n.IL_UNIQUE_ID > {8} ", query);
+    }
+
+    @Test
+    public void testFilterModel_8() {
+        filter_1.setProperty("identifier");
+        filter_1.setOperator(">=");
+        String query = filter_1.getCypher(sc, param);
+        Assert.assertEquals(" n.IL_UNIQUE_ID >= {9} ", query);
+    }
+
+    @Test
+    public void testFilterModel_9() {
+        filter_1.setProperty("identifier");
+        filter_1.setOperator("<");
+        String query = filter_1.getCypher(sc, param);
+        Assert.assertEquals(" n.IL_UNIQUE_ID < {10} ", query);
+    }
+
+    @Test
+    public void testFilterModel_10() {
+        filter_1.setProperty("identifier");
+        filter_1.setOperator("<=");
+        String query = filter_1.getCypher(sc, param);
+        Assert.assertEquals(" n.IL_UNIQUE_ID <= {1} ", query);
+    }
+
+    @Test
+    public void testFilterModel_11() {
+        filter_1.setProperty("identifier");
+        filter_1.setOperator("!=");
+        String query = filter_1.getCypher(sc, param);
+        Assert.assertEquals(" NOT n.IL_UNIQUE_ID = {2} ", query);
+    }
+
+    @Test
+    public void testFilterModel_12() {
+        filter_1.setProperty("identifier");
+        filter_1.setOperator("in");
+        String query = filter_1.getCypher(sc, param);
+        Assert.assertEquals(" n.IL_UNIQUE_ID in {3} ", query);
     }
 
 }
