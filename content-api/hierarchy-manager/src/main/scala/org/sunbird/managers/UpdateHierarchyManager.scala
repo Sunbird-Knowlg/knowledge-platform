85651143cf450cfdd46ccbdb68a46ce55d779807
rheafernandes <rheafernandeshallu@gmail.com>
2020-09-21 16:08:47 +0530

    Fixed object type mapping for resources

--- content-api/hierarchy-manager/src/main/scala/org/sunbird/managers/UpdateHierarchyManager.scala
+++ content-api/hierarchy-manager/src/main/scala/org/sunbird/managers/UpdateHierarchyManager.scala
@@ -404,6 +404,9 @@ object UpdateHierarchyManager {
         tempNode.getMetadata.put(HierarchyConstants.DEPTH, depth.asInstanceOf[AnyRef])
         tempNode.getMetadata.put(HierarchyConstants.PARENT, parent.replaceAll(".img", ""))
         tempNode.getMetadata.put(HierarchyConstants.INDEX, index.asInstanceOf[AnyRef])
+        //Added content mapping as well as object type mapping
+        HierarchyBackwardCompatibilityUtil.setContentAndCategoryTypes(tempNode.getMetadata)
+        HierarchyBackwardCompatibilityUtil.setNewObjectType(tempNode.getMetadata)
     }
 
     /**
