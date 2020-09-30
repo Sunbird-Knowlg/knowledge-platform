3bba617c346047375069089ad8b45dc176580c0a
rheafernandes <rheafernandeshallu@gmail.com>
2020-09-30 17:32:32 +0530

    Added testcases for storage service

--- platform-modules/mimetype-manager/src/test/scala/org/sunbird/cloudstore/StorageServiceTest.scala
+++ platform-modules/mimetype-manager/src/test/scala/org/sunbird/cloudstore/StorageServiceTest.scala
@@ -1,5 +1,17 @@
 package org.sunbird.cloudstore
 
-class StorageServiceTest {
+import org.scalatest.{AsyncFlatSpec, Matchers}
 
+class StorageServiceTest extends AsyncFlatSpec with Matchers {
+    val ss = new StorageService
+
+    "getService" should "return a Storage Service" in {
+        val service = ss.getService()
+        assert(service != null)
+    }
+
+    "getContainerName" should "return the container name" in {
+        val container = ss.getContainerName()
+        assert(container == "sunbird-content-dev")
+    }
 }
