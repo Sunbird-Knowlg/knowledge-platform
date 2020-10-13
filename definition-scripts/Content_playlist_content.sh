476a91a07ff22bfc4a7083dfe3493392da364487
Amit Priyadarshi <darshi.1983@gmail.com>
2020-10-13 17:33:49 +0530

    Issue #000 feat: ObjectCategoryDefinition scripts enhancement - remove mimeType and audience

--- definition-scripts/Content_playlist_content.sh
+++ definition-scripts/Content_playlist_content.sh
@@ -7,29 +7,7 @@ curl -L -X POST '{{host}}/object/category/definition/v4/create' \
       "targetObjectType": "Content",
       "objectMetadata": {
         "config": {},
-        "schema": {
-          "properties": {
-            "audience": {
-              "type": "array",
-              "items": {
-                "type": "string",
-                "enum": [
-                  "Student",
-                  "Teacher"
-                ]
-              },
-              "default": [
-                "Student"
-              ]
-            },
-            "mimeType": {
-              "type": "string",
-              "enum": [
-                "application/vnd.ekstep.content-collection"
-              ]
-            }
-          }
-        }
+        "schema": {}
       }
     }
   }
