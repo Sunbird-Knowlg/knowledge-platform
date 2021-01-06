d703df892106f63b5f330cbfb5ab82b732742302
rheafernandes <rheafernandeshallu@gmail.com>
2021-01-06 16:01:58 +0530

    Added requested changes

--- assessment-api/assessment-actors/src/main/scala/org/sunbird/managers/QuestionManager.scala
+++ assessment-api/assessment-actors/src/main/scala/org/sunbird/managers/QuestionManager.scala
@@ -115,10 +115,10 @@ object QuestionManager {
     def validateQuestionSetHierarchy(hierarchyString: String)(implicit ec: ExecutionContext, oec: OntologyEngineContext): Unit = {
         if (!skipValidation) {
             val hierarchy = if (!hierarchyString.asInstanceOf[String].isEmpty) {
-                JsonUtils.deserialize(hierarchyString.asInstanceOf[String], classOf[java.util.HashMap[String, AnyRef]])
+                JsonUtils.deserialize(hierarchyString.asInstanceOf[String], classOf[java.util.Map[String, AnyRef]])
             } else
                 new java.util.HashMap[String, AnyRef]()
-            val children = hierarchy.getOrDefault("children", new util.ArrayList[java.util.Map[String, AnyRef]]).asInstanceOf[util.ArrayList[java.util.Map[String, AnyRef]]]
+            val children = hierarchy.getOrDefault("children", new util.ArrayList[java.util.Map[String, AnyRef]]).asInstanceOf[util.List[java.util.Map[String, AnyRef]]]
             validateChildrenRecursive(children)
         }
     }
