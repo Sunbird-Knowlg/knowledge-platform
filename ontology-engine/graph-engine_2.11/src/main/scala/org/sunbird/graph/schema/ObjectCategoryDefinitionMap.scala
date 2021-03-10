963a8b60d6608005c69407ce3741717bb36734d6
Mahesh Kumar Gangula <maheshkumargangula@users.noreply.github.com>
2021-03-10 14:28:12 +0530

    Issue #SB-23358 feat: category cache test cases.

--- ontology-engine/graph-engine_2.11/src/main/scala/org/sunbird/graph/schema/ObjectCategoryDefinitionMap.scala
+++ ontology-engine/graph-engine_2.11/src/main/scala/org/sunbird/graph/schema/ObjectCategoryDefinitionMap.scala
@@ -6,15 +6,16 @@ import org.sunbird.common.{Platform, Slug}
 
 object ObjectCategoryDefinitionMap {
 
-  val ttlMS = Platform.getLong("object.categoryDefinition.cache.ttl", 600000l)
-  val cache =  Cache.ttl[String, Map[String, AnyRef]](Duration.fromMilliseconds(ttlMS))
+  val ttlMS = Platform.getLong("object.categoryDefinition.cache.ttl", 10000l)
+  var cache =  Cache.ttl[String, Map[String, AnyRef]](Duration.fromMilliseconds(ttlMS))
     
   def get(id: String):Map[String, AnyRef] = {
     cache.getNonExpired(id).getOrElse(null)
   }
 
   def put(id: String, data: Map[String, AnyRef]): Unit = {
-    cache.putClocked(id, data)
+    val updated = cache.putClocked(id, data)._2
+    cache = updated
   }
 
   def containsKey(id: String): Boolean = {
