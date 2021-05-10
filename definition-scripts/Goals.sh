260c2de98c29853872ed2e83559a82d62a7927b2
amit-kumaryadav <amtydv630@gmail.com>
2021-05-10 11:51:58 +0530

    Playlist, Goals config changes

---
+++ definition-scripts/Goals.sh
@@ -0,0 +1,52 @@
+curl -L -X POST '{{host}}/object/category/definition/v4/create' \
+-H 'Content-Type: application/json' \
+--data-raw {
+  "request": {
+    "objectCategoryDefinition": {
+      "categoryId": "obj-cat:goals",
+      "targetObjectType": "Collection",
+      "objectMetadata": {
+        "config": {},
+        "schema": {
+          "properties": {
+           "visibility": {
+            "type": "string",
+            "enum": [
+                "Default",
+                "Parent",
+                "Private"
+            ],
+            "default": "Private"
+           },
+            "trackable": {
+              "type": "object",
+              "properties": {
+                "enabled": {
+                  "type": "string",
+                  "enum": [
+                    "Yes",
+                    "No"
+                  ],
+                  "default": "Yes"
+                },
+                "autoBatch": {
+                  "type": "string",
+                  "enum": [
+                    "Yes",
+                    "No"
+                  ],
+                  "default": "Yes"
+                }
+              },
+              "default": {
+                "enabled": "Yes",
+                "autoBatch": "Yes"
+              },
+              "additionalProperties": false
+            }
+          }
+        }
+      }
+    }
+  }
+}'
