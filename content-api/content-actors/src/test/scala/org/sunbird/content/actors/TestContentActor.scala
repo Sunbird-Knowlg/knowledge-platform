7bd1ccedc311193a0684abbec1e6d7261bd7c653
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2021-08-16 19:14:52 +0530

    Issue #SB-14589 fix: Reject reasons capture error fix.

--- content-api/content-actors/src/test/scala/org/sunbird/content/actors/TestContentActor.scala
+++ content-api/content-actors/src/test/scala/org/sunbird/content/actors/TestContentActor.scala
@@ -505,25 +505,6 @@ class TestContentActor extends BaseSpec with MockFactory {
         assert("Content metadata error, status is blank for identifier:do_1234".equals(response.getParams.getErrmsg))
     }
 
-    it should "return client exception for 'rejectContent' with invalid rejectReasons format" in {
-        implicit val oec: OntologyEngineContext = mock[OntologyEngineContext]
-        val graphDB = mock[GraphService]
-        (oec.graphService _).expects().returns(graphDB).anyNumberOfTimes()
-        implicit val ss = mock[StorageService]
-        val node =  getValidNodeToReject()
-        (graphDB.getNodeByUniqueId(_: String, _: String, _: Boolean, _: Request)).expects(*, *, *, *).returns(Future(node)).anyNumberOfTimes()
-        (graphDB.getNodeProperty(_: String, _: String, _: String)).expects(*, *, *).returns(Future(new Property("versionKey", new org.neo4j.driver.internal.value.StringValue("test_123")))).anyNumberOfTimes()
-        (graphDB.upsertNode(_: String, _: Node, _: Request)).expects(*, *, *).returns(Future(node)).anyNumberOfTimes()
-        val request = getContentRequest()
-        request.getContext.put("identifier","do_1234")
-        request.putAll(mapAsJavaMap(Map("description" -> "test desc", "versionKey" -> "test_123","rejectReasons" -> "Incorrect Content")))
-        request.setOperation("rejectContent")
-        val response = callActor(request, Props(new ContentActor()))
-        assert("failed".equals(response.getParams.getStatus))
-        assert("ERR_INVALID_REQUEST_FORMAT".equals(response.getParams.getErr))
-        assert("rejectReasons should be a Array".equals(response.getParams.getErrmsg))
-    }
-
     private def getAssetNodeToUpload(): Node = {
         val node = new Node()
         node.setIdentifier("do_1234")
