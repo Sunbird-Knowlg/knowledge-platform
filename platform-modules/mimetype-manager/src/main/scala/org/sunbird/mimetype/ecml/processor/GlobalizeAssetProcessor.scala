b616605fdfd30331bddfeb8070c16e76752561a3
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2021-08-26 18:18:10 +0530

    Issue #SB-26351 fix: Adding logs to debug issue.

--- platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/ecml/processor/GlobalizeAssetProcessor.scala
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/ecml/processor/GlobalizeAssetProcessor.scala
@@ -41,13 +41,12 @@ trait GlobalizeAssetProcessor extends IProcessor {
                             val blobUrl = if (!(mediaSrc.startsWith("http"))) {
                                 if (mediaSrc.startsWith("/")) BASE_URL + mediaSrc else BASE_URL + File.separator + mediaSrc
                             } else mediaSrc
-
-                          TelemetryManager.info("GlobalizeAssetProcessor::uploadAssets:: uploading file:: "+file.getAbsolutePath)
+                            TelemetryManager.info("GlobalizeAssetProcessor::uploadAssets:: mediaSrc: " + mediaSrc + " | uploading file: "+file.getAbsolutePath)
                             val uploadFileUrl: Array[String] = if (StringUtils.isNoneBlank(cloudDirName) && getBlobLength(blobUrl) == 0) ss.uploadFile(cloudDirName, file)
                             else new Array[String](1)
-                          TelemetryManager.info("GlobalizeAssetProcessor::uploadAssets:: uploadFileUrl:: "+uploadFileUrl)
 
                             if (null != uploadFileUrl && uploadFileUrl.size > 1) {
+                              TelemetryManager.info("GlobalizeAssetProcessor::uploadAssets:: uploadFileUrl:: "+uploadFileUrl(1))
                                 if (!(mediaSrc.startsWith("http") || mediaSrc.startsWith("/"))) {
                                     val temp = media.data ++ Map("src" -> ("/" + mediaSrc))
                                     Media(media.id, temp, media.innerText, media.cData, uploadFileUrl(1), media.`type`, media.childrenPlugin)
