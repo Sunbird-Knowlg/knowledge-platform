ffe91b620c3998904699f7156300619c5e1a5f7f
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2021-09-01 17:12:26 +0530

    Update SearchActor.java

--- search-api/search-actors/src/main/java/org/sunbird/actors/SearchActor.java
+++ search-api/search-actors/src/main/java/org/sunbird/actors/SearchActor.java
@@ -586,7 +586,7 @@ public class SearchActor extends SearchBaseActor {
                         if (obj instanceof Map) {
                             Map<String, Object> map = (Map<String, Object>) obj;
                             if (StringUtils.equalsIgnoreCase((String) map.getOrDefault("visibility",""),"Private")){
-                                throw new ClientException("ERR_ACCESS_DENIED","Cannot access private data, hence access denied for identifier: " + (String) map.get("identifier"));
+                                throw new ClientException("ERR_ACCESS_DENIED","Cannot access private data, hence access denied for identifier: " + map.get("identifier"));
                             }
                             String objectType = ((String) map.getOrDefault("objectType", "")).replaceAll("Image", "");
                             if(StringUtils.equalsIgnoreCase("Collection", objectType) || StringUtils.equalsIgnoreCase("Asset", objectType))
