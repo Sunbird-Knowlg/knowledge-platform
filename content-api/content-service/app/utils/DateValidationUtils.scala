5907b8d091222a365145348a7780fda3bbb8cbf1
GauravTekdi <gaurav_l@techjoomla.com>
2021-09-17 11:38:41 +0530

    Handled situation of startTime or endTime empty or null while validating, and removed unnecessary semicolons.

--- content-api/content-service/app/utils/DateValidationUtils.scala
+++ content-api/content-service/app/utils/DateValidationUtils.scala
@@ -1,5 +1,7 @@
 package utils
 
+import org.apache.commons.lang3.StringUtils
+
 import java.text.{DateFormat, SimpleDateFormat}
 import java.util.Date
 
@@ -12,20 +14,22 @@ object DateValidationUtils {
    * @return true : If endDate is > startDate; false : otherwise.
    */
   def validateDatesAndTimes(input: java.util.Map[String, AnyRef]): Boolean = {
-
-    val startDateString : String = input.getOrDefault("startDate", dateToStringConverter(new Date())).toString;
-    val startDate : java.util.Date = stringToDateConverter(startDateString);
-    val endDateString : String = input.getOrDefault("endDate", dateToStringConverter(new Date())).toString;
-    val endDate : java.util.Date = stringToDateConverter(endDateString);
-
-    if (startDate.equals(endDate)) {
-      val startTimeString : String = input.getOrDefault("startTime", "00:00:00+00:00").toString;
-      val startTime : Date = stringToTimeConverter(startTimeString);
-      val endTimeString : String = input.getOrDefault("endTime", "00:00:00+00:00").toString;
-      val endTime : Date = stringToTimeConverter(endTimeString);
-      return (startTime.after(endTime) || startTime.equals(endTime));
+    val startDateString: String = input.get("startDate").asInstanceOf[String]
+    val endDateString: String = input.get("endDate").asInstanceOf[String]
+    if (StringUtils.isNotBlank(startDateString) && StringUtils.isNotBlank(endDateString)) {
+      val startDate: java.util.Date = stringToDateConverter(startDateString)
+      val endDate: java.util.Date = stringToDateConverter(endDateString)
+      val startTimeString: String = input.get("startTime").asInstanceOf[String]
+      val endTimeString: String = input.get("endTime").asInstanceOf[String]
+      if (startDate.equals(endDate) && StringUtils.isNotBlank(startTimeString) && StringUtils.isNotBlank(endTimeString)) {
+        val startTime: Date = stringToTimeConverter(startTimeString)
+        val endTime: Date = stringToTimeConverter(endTimeString)
+        return (startTime.after(endTime) || startTime.equals(endTime))
+      }
+      startDate.after(endDate)
+    } else {
+      false
     }
-    return startDate.after(endDate);
   }
 
   /**
@@ -34,10 +38,9 @@ object DateValidationUtils {
    * @param dateString the date in String format.
    * @return Date in java.util.Date format.
    */
-  def stringToDateConverter(dateString : String): java.util.Date ={
-
+  def stringToDateConverter(dateString: String): java.util.Date = {
     val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
-    dateFormat.parse(dateString);
+    dateFormat.parse(dateString)
   }
 
   /**
@@ -46,11 +49,10 @@ object DateValidationUtils {
    * @param date the date in java.util.Date format.
    * @return Date in String format.
    */
-  def dateToStringConverter(date : java.util.Date): String ={
-
-    val dateFormat : DateFormat = new SimpleDateFormat("yyyy-mm-dd");
-    val strDate : String = dateFormat.format(date);
-    return strDate;
+  def dateToStringConverter(date: java.util.Date): String = {
+    val dateFormat: DateFormat = new SimpleDateFormat("yyyy-mm-dd")
+    val strDate: String = dateFormat.format(date)
+    strDate
   }
 
   /**
@@ -59,8 +61,7 @@ object DateValidationUtils {
    * @param timeString the date and time in String format.
    * @return Date in java.util.Date format.
    */
-  def stringToTimeConverter(timeString : String): java.util.Date ={
-
+  def stringToTimeConverter(timeString: String): java.util.Date = {
     val dateFormat = new SimpleDateFormat("HH:mm:ss")
     dateFormat.parse(timeString);
   }
