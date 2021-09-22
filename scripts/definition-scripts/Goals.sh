9b87b9ca9e6e55f13eace20a196bcf210e6f8b3b
Anil Gupta <ani2824@gmail.com>
2021-09-22 08:48:28 +0530

    Issue #SB-0000 fix: Fixed failed scripts.

--- scripts/definition-scripts/Goals.sh
+++ scripts/definition-scripts/Goals.sh
@@ -1,6 +1,6 @@
 curl -L -X POST '{{host}}/object/category/definition/v4/create' \
 -H 'Content-Type: application/json' \
---data-raw {
+--data-raw '{
   "request": {
     "objectCategoryDefinition": {
       "categoryId": "obj-cat:goals",
