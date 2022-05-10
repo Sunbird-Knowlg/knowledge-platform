8978b8dc5fd0d1b644487447ed1b75fcea7a89a9
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2022-05-10 16:08:18 +0530

    Issue #SB-29808 debug: Review API failing

--- platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/ecml/processor/LocalizeAssetProcessor.scala
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/ecml/processor/LocalizeAssetProcessor.scala
@@ -7,7 +7,6 @@ import org.apache.commons.lang3.StringUtils
 import org.sunbird.cloudstore.StorageService
 import org.sunbird.common.Platform
 import org.sunbird.common.exception.ClientException
-import org.sunbird.telemetry.logger.TelemetryManager
 
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration.Duration
@@ -57,9 +56,8 @@ trait LocalizeAssetProcessor extends IProcessor {
 
 	def getUrlWithPrefix(src: String) = {
 		if (StringUtils.isNotBlank(src) && !src.startsWith("http")) {
-			if (src.contains("content-plugins/")) PLUGIN_MEDIA_BASE_URL + File.separator + src
-			else if(src.startsWith("assets")) CONTENT_MEDIA_BASE_URL + File.separator + src
-			else CONTENT_MEDIA_BASE_URL + src
+			if(!src.startsWith("/")) PLUGIN_MEDIA_BASE_URL + File.separator + src
+			else PLUGIN_MEDIA_BASE_URL + src
 		} else src
 	}
 
