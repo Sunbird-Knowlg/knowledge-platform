9c4884d57e58f597dc34f34e3b337bcb129b82af
AmiableAnil <ani2824@gmail.com>
2022-09-01 16:55:33 +0530

    Issue #KN-231 test: Improved unit test coverage.

--- platform-modules/mimetype-manager/src/test/scala/org/sunbird/cloudstore/StorageServiceTest.scala
+++ platform-modules/mimetype-manager/src/test/scala/org/sunbird/cloudstore/StorageServiceTest.scala
@@ -2,6 +2,8 @@ package org.sunbird.cloudstore
 
 import org.scalatest.{AsyncFlatSpec, Matchers}
 
+import java.io.File
+
 class StorageServiceTest extends AsyncFlatSpec with Matchers {
     val ss = new StorageService
 
@@ -13,5 +15,16 @@ class StorageServiceTest extends AsyncFlatSpec with Matchers {
     "getContainerName" should "return the container name" in {
         val container = ss.getContainerName
         assert(container == "sunbird-content-dev")
+    }
+
+    "getSignedURL" should "return the signed url" in {
+        val objectKey = "content" + File.separator + "asset" + File.separator + "do_53245" + File.separator + "abc.png"
+        val preSignedURL = ss.getSignedURL(objectKey, Option.apply(600), Option.apply("w"), Option.apply(""))
+        assert(preSignedURL.contains(objectKey))
+    }
+
+    "getUri" should "return the signed url" in {
+        val uri = ss.getUri("content/abc.json")
+        assert(uri != null)
     }
 }
