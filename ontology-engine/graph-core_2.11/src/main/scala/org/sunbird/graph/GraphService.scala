10fd889335ae6551ed536ea1a4956a0e7a174e7a
anilgupta <anil.gupta@tarento.com>
2022-11-22 21:51:20 +0530

    Issue #KN-439 feat: Handles domain agnostic for Neo4J.

--- ontology-engine/graph-core_2.11/src/main/scala/org/sunbird/graph/GraphService.scala
+++ ontology-engine/graph-core_2.11/src/main/scala/org/sunbird/graph/GraphService.scala
@@ -1,12 +1,11 @@
 package org.sunbird.graph
 
-import java.util
-
 import org.sunbird.common.dto.{Property, Request, Response}
 import org.sunbird.graph.dac.model.{Node, SearchCriteria}
 import org.sunbird.graph.external.ExternalPropsManager
 import org.sunbird.graph.external.store.ExternalStore
 import org.sunbird.graph.service.operation.{GraphAsyncOperations, Neo4JBoltSearchOperations, NodeAsyncOperations, SearchAsyncOperations}
+import org.sunbird.graph.util.CSPMetaUtil
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -14,10 +13,14 @@ class GraphService {
     implicit  val ec: ExecutionContext = ExecutionContext.global
 
     def addNode(graphId: String, node: Node): Future[Node] = {
+        val metadata = CSPMetaUtil.updateRelativePath(node.getMetadata)
+        node.setMetadata(metadata)
         NodeAsyncOperations.addNode(graphId, node)
     }
 
     def upsertNode(graphId: String, node: Node, request: Request): Future[Node] = {
+        val metadata = CSPMetaUtil.updateRelativePath(node.getMetadata)
+        node.setMetadata(metadata)
         NodeAsyncOperations.upsertNode(graphId, node, request)
     }
 
@@ -26,7 +29,7 @@ class GraphService {
     }
 
     def getNodeByUniqueId(graphId: String, nodeId: String, getTags: Boolean, request: Request): Future[Node] = {
-        SearchAsyncOperations.getNodeByUniqueId(graphId, nodeId, getTags, request)
+        SearchAsyncOperations.getNodeByUniqueId(graphId, nodeId, getTags, request).map(node => CSPMetaUtil.updateAbsolutePath(node))
     }
 
     def deleteNode(graphId: String, nodeId: String, request: Request): Future[java.lang.Boolean] = {
@@ -34,14 +37,15 @@ class GraphService {
     }
 
     def getNodeProperty(graphId: String, identifier: String, property: String): Future[Property] = {
-        SearchAsyncOperations.getNodeProperty(graphId, identifier, property)
+        SearchAsyncOperations.getNodeProperty(graphId, identifier, property).map(property => CSPMetaUtil.updateAbsolutePath(property))
     }
-    def updateNodes(graphId: String, identifiers:util.List[String], metadata:util.Map[String,AnyRef]):Future[util.Map[String, Node]] = {
-        NodeAsyncOperations.updateNodes(graphId, identifiers, metadata)
+    def updateNodes(graphId: String, identifiers:java.util.List[String], metadata:java.util.Map[String,AnyRef]):Future[java.util.Map[String, Node]] = {
+        val updatedMetadata = CSPMetaUtil.updateRelativePath(metadata)
+        NodeAsyncOperations.updateNodes(graphId, identifiers, updatedMetadata)
     }
 
-    def getNodeByUniqueIds(graphId:String, searchCriteria: SearchCriteria): Future[util.List[Node]] = {
-        SearchAsyncOperations.getNodeByUniqueIds(graphId, searchCriteria)
+    def getNodeByUniqueIds(graphId:String, searchCriteria: SearchCriteria): Future[java.util.List[Node]] = {
+        SearchAsyncOperations.getNodeByUniqueIds(graphId, searchCriteria).map(nodes => CSPMetaUtil.updateAbsolutePath(nodes))
     }
 
     def readExternalProps(request: Request, fields: List[String]): Future[Response] = {
@@ -63,11 +67,11 @@ class GraphService {
         Neo4JBoltSearchOperations.checkCyclicLoop(graphId, endNodeId, relationType, startNodeId)
     }
 
-    def removeRelation(graphId: String, relationMap: util.List[util.Map[String, AnyRef]]) = {
+    def removeRelation(graphId: String, relationMap: java.util.List[java.util.Map[String, AnyRef]]) = {
         GraphAsyncOperations.removeRelation(graphId, relationMap)
     }
 
-    def createRelation(graphId: String, relationMap: util.List[util.Map[String, AnyRef]]) = {
+    def createRelation(graphId: String, relationMap: java.util.List[java.util.Map[String, AnyRef]]) = {
         GraphAsyncOperations.createRelation(graphId, relationMap)
     }
 }
---
+++ ontology-engine/graph-core_2.11/src/main/scala/org/sunbird/graph/util/CSPMetaUtil.scala
@@ -0,0 +1,73 @@
+package org.sunbird.graph.util
+
+import org.apache.commons.collections4.MapUtils
+import org.slf4j.LoggerFactory
+import org.sunbird.common.Platform
+import org.sunbird.common.dto.Property
+import org.sunbird.graph.dac.model.Node
+
+import scala.collection.JavaConverters._
+
+object CSPMetaUtil {
+  private[this] val logger = LoggerFactory.getLogger(classOf[CSPMetaUtil])
+  private val defaultMetadataList = java.util.Arrays.asList("appIcon","posterImage","artifactUrl","downloadUrl","variants","previewUrl","pdfUrl")
+  private val defaultWriteBasePath = java.util.Arrays.asList("https://sunbirddev.blob.core.windows.net","https://obj.dev.sunbird.org")
+
+  def updateAbsolutePath(data: java.util.Map[String, AnyRef]): java.util.Map[String, AnyRef] = {
+    val cspMeta = Platform.getStringList("cloudstorage.metadata.list", defaultMetadataList).asScala.toList
+    val absolutePath = Platform.getString("cloudstorage.read_base_path", "") + java.io.File.separator + Platform.getString("cloud_storage_container", "")
+    if (MapUtils.isNotEmpty(data)) {
+      val updatedMeta: java.util.Map[String, AnyRef] = data.asScala.map(x => if (cspMeta.contains(x._1)) (x._1, x._2.asInstanceOf[String].replace("CLOUD_STORAGE_BASE_PATH", absolutePath)) else (x._1, x._2)).toMap.asJava
+      updatedMeta
+    } else data
+  }
+
+  def updateAbsolutePath(node: Node): Node = {
+    val metadata = updateAbsolutePath(node.getMetadata)
+    node.setMetadata(metadata)
+    node
+  }
+
+  def updateAbsolutePath(nodes: java.util.List[Node]): java.util.List[Node] = {
+    nodes.asScala.toList.map(node => {
+      updateAbsolutePath(node)
+    }).asJava
+  }
+
+  def updateAbsolutePath(property: Property): Property = {
+    val cspMeta = Platform.getStringList("cloudstorage.metadata.list", defaultMetadataList)
+    val absolutePath = Platform.getString("cloudstorage.read_base_path", "") + java.io.File.separator + Platform.getString("cloud_storage_container", "")
+    if(cspMeta.contains(property.getPropertyName)) {
+      val value = property.getPropertyValue
+      value match {
+        case str: String =>
+          property.setPropertyValue(str.replace("CLOUD_STORAGE_BASE_PATH", absolutePath))
+        case _ =>
+      }
+    }
+    property
+  }
+
+//  def updateRelativePath(data: java.util.Map[String, AnyRef]): java.util.Map[String, AnyRef] = {
+//    val cspMeta = Platform.getStringList("cloudstorage.metadata.list", defaultMetadataList)
+//    val validCSPSource: java.util.List[String] = Platform.getStringList("cloudstorage.write_base_path", defaultWriteBasePath)
+//    val basePath: List[String] = validCSPSource.asScala.toList.map(source => source + java.io.File.separator + Platform.getString("cloud_storage_container", ""))
+//    if (MapUtils.isNotEmpty(data)) {
+//      val updatedMeta: java.util.Map[String, AnyRef] = data.asScala.map(x => if (cspMeta.contains(x._1)) (x._1, basePath.map(path => {if (x._2.asInstanceOf[String].contains(path)) x._2.asInstanceOf[String].replace(path, "CLOUD_STORAGE_BASE_PATH") else x._2 })) else (x._1, x._2)).toMap.asJava
+//      updatedMeta
+//    } else data
+//  }
+
+  def updateRelativePath(data: java.util.Map[String, AnyRef]): java.util.Map[String, AnyRef] = {
+    val cspMeta: java.util.List[String] = Platform.getStringList("cloudstorage.metadata.list", defaultMetadataList)
+    val validCSPSource: java.util.List[String] = Platform.getStringList("cloudstorage.write_base_path", defaultWriteBasePath)
+    val basePath: List[String] = validCSPSource.asScala.toList.map(source => source + java.io.File.separator + Platform.getString("cloud_storage_container", ""))
+    if (MapUtils.isNotEmpty(data)) {
+      val updatedMeta: java.util.Map[String, AnyRef] = data.asScala.map(x => if (cspMeta.contains(x._1)) (x._1, basePath.map(path => if (x._2.asInstanceOf[String].contains(path)) x._2.asInstanceOf[String].replace(path, "CLOUD_STORAGE_BASE_PATH") else x._2)) else (x._1, x._2)).toMap.asJava
+      updatedMeta
+    } else data
+  }
+
+}
+
+class CSPMetaUtil {}
