ea61954bf7a2b936ee8544fd596451a756d42c16
Kartheek Palla <kartheek@sanketika.in>
2023-05-22 18:09:19 +0530

    Issue #KN-129 fix: fixed PR review comments

--- taxonomy-api/taxonomy-actors/src/main/scala/org/sunbird/actors/CategoryInstanceActor.scala
+++ taxonomy-api/taxonomy-actors/src/main/scala/org/sunbird/actors/CategoryInstanceActor.scala
@@ -8,7 +8,6 @@ import org.sunbird.common.exception.{ClientException, ResourceNotFoundException}
 import org.sunbird.graph.OntologyEngineContext
 import org.sunbird.graph.nodes.DataNode
 import org.sunbird.graph.utils.NodeUtil
-import org.sunbird.mangers.CategoryInstanceManager
 import org.sunbird.utils.{Constants, RequestUtil}
 
 import java.util
@@ -36,14 +35,14 @@ class CategoryInstanceActor @Inject()(implicit oec: OntologyEngineContext) exten
     if (!request.getRequest.containsKey(Constants.CODE)) throw new ClientException("ERR_CATEGORY_CODE_REQUIRED", "Unique code is mandatory for categoryInstance")
     val getCategoryReq = new Request()
     getCategoryReq.setContext(new util.HashMap[String, AnyRef]() {{
-        putAll(request.getContext)
+      putAll(request.getContext)
     }})
     getCategoryReq.getContext.put(Constants.SCHEMA_NAME, Constants.FRAMEWORK_SCHEMA_NAME)
     getCategoryReq.getContext.put(Constants.VERSION, Constants.FRAMEWORK_SCHEMA_VERSION)
     getCategoryReq.put(Constants.IDENTIFIER, frameworkId)
     DataNode.read(getCategoryReq).map(node => {
       if (null != node && StringUtils.equalsAnyIgnoreCase(node.getIdentifier, frameworkId)) {
-        request.getRequest.put(Constants.IDENTIFIER, CategoryInstanceManager.generateIdentifier(frameworkId, code))
+        request.getRequest.put(Constants.IDENTIFIER, generateIdentifier(frameworkId, code))
         DataNode.create(request).map(node => {
           ResponseHandler.OK.put(Constants.IDENTIFIER, node.getIdentifier)
             .put(Constants.VERSION_KEY, node.getMetadata.get("versionKey"))
@@ -53,13 +52,36 @@ class CategoryInstanceActor @Inject()(implicit oec: OntologyEngineContext) exten
   }
 
   private def read(request: Request): Future[Response] = {
-    val requestMethod = request.getRequest.getOrDefault("REQ_METHOD", "").asInstanceOf[String]
+    validateCategoryInstanceObject(request).map(node => {
+      val metadata: util.Map[String, AnyRef] = NodeUtil.serialize(node, null, request.getContext.get("schemaName").asInstanceOf[String], request.getContext.get("version").asInstanceOf[String])
+      ResponseHandler.OK.put("categoryInstance", metadata)
+    } )
+  }
+
+  private def update(request: Request): Future[Response] = {
     RequestUtil.restrictProperties(request)
+    request.getRequest.put(Constants.IDENTIFIER, generateIdentifier(request.getRequest.getOrDefault(Constants.FRAMEWORK, "").asInstanceOf[String], request.getRequest.getOrDefault(Constants.IDENTIFIER, "").asInstanceOf[String]))
+    validateCategoryInstanceObject(request)
+    DataNode.update(request).map(node => {
+      ResponseHandler.OK.put(Constants.IDENTIFIER, node.getIdentifier).put(Constants.VERSION_KEY, node.getMetadata.get("versionKey"))
+    })
+  }
+
+  private def retire(request: Request): Future[Response] = {
+    validateCategoryInstanceObject(request)
+    request.getRequest.put(Constants.IDENTIFIER, generateIdentifier(request.getRequest.getOrDefault(Constants.FRAMEWORK, "").asInstanceOf[String], request.getRequest.getOrDefault(Constants.IDENTIFIER, "").asInstanceOf[String]))
+    request.getRequest.put("status", "Retired")
+    DataNode.update(request).map(node => {
+      ResponseHandler.OK.put(Constants.IDENTIFIER, node.getIdentifier).put(Constants.VERSION_KEY, node.getMetadata.get("versionKey"))
+    })
+  }
+
+  private def validateCategoryInstanceObject(request: Request)(implicit oec: OntologyEngineContext, ec: ExecutionContext) = {
     val frameworkId = request.getRequest.getOrDefault(Constants.FRAMEWORK, "").asInstanceOf[String]
     val categoryId = request.getRequest.getOrDefault(Constants.IDENTIFIER, "").asInstanceOf[String]
     if (frameworkId.isEmpty()) throw new ClientException("ERR_INVALID_FRAMEWORK_ID", s"Invalid FrameworkId: '${frameworkId}' for Categoryinstance ")
     if (categoryId.isEmpty()) throw new ClientException("ERR_INVALID_CATEGORY_ID", s"Invalid CategoryId: '${categoryId}' for categoryInstance")
-    val categoryInstanceId = CategoryInstanceManager.generateIdentifier(frameworkId, categoryId)
+    val categoryInstanceId = generateIdentifier(frameworkId, categoryId)
     val getCategoryReq = new Request()
     getCategoryReq.setContext(new util.HashMap[String, AnyRef]() {
       {
@@ -69,76 +91,16 @@ class CategoryInstanceActor @Inject()(implicit oec: OntologyEngineContext) exten
     getCategoryReq.getContext.put(Constants.SCHEMA_NAME, Constants.CATEGORY_INSTANCE_SCHEMA_NAME)
     getCategoryReq.getContext.put(Constants.VERSION, Constants.CATEGORY_INSTANCE_SCHEMA_VERSION)
     getCategoryReq.put(Constants.IDENTIFIER, categoryInstanceId)
-    DataNode.read(getCategoryReq) recoverWith {
-      case e: ResourceNotFoundException => {
-        if (StringUtils.equalsAnyIgnoreCase("POST", requestMethod)) {
-          DataNode.read(getCategoryReq)
-        } else
-          throw new ClientException("ERR_CHANNEL_NOT_FOUND/ ERR_FRAMEWORK_NOT_FOUND", s"Given channel/framework is not related to given category")
-      }
-      case ex: Throwable => throw ex
-    } map (node => {
-      val metadata: util.Map[String, AnyRef] = NodeUtil.serialize(node, null, request.getContext.get("schemaName").asInstanceOf[String], request.getContext.get("version").asInstanceOf[String])
-      ResponseHandler.OK.put("categoryInstance", metadata)
-    })
+    DataNode.read(getCategoryReq)(oec, ec).map(node => {
+      if (null != node && StringUtils.equalsAnyIgnoreCase(node.getIdentifier, categoryInstanceId)) node
+      else throw new ClientException("ERR_CHANNEL_NOT_FOUND/ ERR_FRAMEWORK_NOT_FOUND", s"Given channel/framework is not related to given category")
+    })(ec)
   }
 
-  private def update(request: Request): Future[Response] = {
-    val requestMethod = request.getRequest.getOrDefault("REQ_METHOD", "").asInstanceOf[String]
-    RequestUtil.restrictProperties(request)
-    val frameworkId = request.getRequest.getOrDefault(Constants.FRAMEWORK, "").asInstanceOf[String]
-    val categoryId = request.getRequest.getOrDefault(Constants.CATEGORY, "").asInstanceOf[String]
-    if (frameworkId.isEmpty()) throw new ClientException("ERR_INVALID_FRAMEWORK_ID", s"Invalid FrameworkId: '${frameworkId}' for Categoryinstance ")
-    if (categoryId.isEmpty()) throw new ClientException("ERR_INVALID_CATEGORY_ID", s"Invalid CategoryId: '${categoryId}' for categoryInstance")
-    val categoryInstanceId = CategoryInstanceManager.generateIdentifier(frameworkId, categoryId)
-    val getCategoryReq = new Request()
-    getCategoryReq.setContext(new util.HashMap[String, AnyRef]() {{
-        putAll(request.getContext)
-    }})
-    getCategoryReq.getContext.put(Constants.SCHEMA_NAME, Constants.CATEGORY_INSTANCE_SCHEMA_NAME)
-    getCategoryReq.getContext.put(Constants.VERSION, Constants.CATEGORY_INSTANCE_SCHEMA_VERSION)
-    getCategoryReq.put(Constants.IDENTIFIER, categoryInstanceId)
-    DataNode.read(getCategoryReq) recoverWith {
-      case e: ResourceNotFoundException => {
-        if (StringUtils.equalsAnyIgnoreCase("PATCH", requestMethod)) {
-          request.getRequest.put(Constants.IDENTIFIER, CategoryInstanceManager.generateIdentifier(frameworkId, categoryId))
-          DataNode.update(request)
-        } else
-          throw new ClientException("ERR_CHANNEL_NOT_FOUND/ ERR_FRAMEWORK_NOT_FOUND", s"Given channel/framework is not related to given category")
-      }
-      case ex: Throwable => throw ex
-    } map (node => {
-      ResponseHandler.OK.put(Constants.IDENTIFIER, node.getIdentifier).put(Constants.VERSION_KEY, node.getMetadata.get("versionKey"))
-    })
-  }
-
-  private def retire(request: Request): Future[Response] = {
-    val requestMethod = request.getRequest.getOrDefault("REQ_METHOD", "").asInstanceOf[String]
-    val frameworkId = request.getRequest.getOrDefault(Constants.FRAMEWORK, "").asInstanceOf[String]
-    val categoryId = request.getRequest.getOrDefault(Constants.IDENTIFIER, "").asInstanceOf[String]
-    if (frameworkId.isEmpty()) throw new ClientException("ERR_INVALID_FRAMEWORK_ID", s"Invalid FrameworkId: '${frameworkId}' for Categoryinstance ")
-    if (categoryId.isEmpty()) throw new ClientException("ERR_INVALID_CATEGORY_ID", s"Invalid CategoryId: '${categoryId}' for categoryInstance")
-    val categoryInstanceId = CategoryInstanceManager.generateIdentifier(frameworkId, categoryId)
-    val getCategoryReq = new Request()
-    getCategoryReq.setContext(new util.HashMap[String, AnyRef]() {{
-        putAll(request.getContext)
-    }})
-    getCategoryReq.getContext.put(Constants.SCHEMA_NAME, Constants.CATEGORY_INSTANCE_SCHEMA_NAME)
-    getCategoryReq.getContext.put(Constants.VERSION, Constants.CATEGORY_INSTANCE_SCHEMA_VERSION)
-    getCategoryReq.put(Constants.IDENTIFIER, categoryInstanceId)
-    DataNode.read(getCategoryReq) recoverWith {
-      case e: ResourceNotFoundException => {
-        if (StringUtils.equalsAnyIgnoreCase("DELETE", requestMethod)) {
-          request.getRequest.put(Constants.IDENTIFIER, CategoryInstanceManager.generateIdentifier(frameworkId, categoryId))
-          request.getRequest.put("status", "Retired")
-          DataNode.update(request)
-        } else
-          throw new ClientException("ERR_CHANNEL_NOT_FOUND/ ERR_FRAMEWORK_NOT_FOUND", s"Given channel/framework is not related to given category")
-      }
-      case ex: Throwable => throw ex
-    } map (node => {
-      ResponseHandler.OK.put(Constants.IDENTIFIER, node.getIdentifier).put(Constants.NODE_ID, node.getIdentifier)
-    })
+  private def generateIdentifier(scopeId: String, code: String): String = {
+    var id: String = null
+    if (StringUtils.isNotBlank(scopeId)) id = Slug.makeSlug(scopeId + "_" + code)
+    id
   }
 
 }
--- taxonomy-api/taxonomy-actors/src/main/scala/org/sunbird/mangers/CategoryManager.scala
+++ taxonomy-api/taxonomy-actors/src/main/scala/org/sunbird/mangers/CategoryManager.scala
@@ -1,5 +1,6 @@
 package org.sunbird.mangers
 
+import com.twitter.util.Config.intoOption
 import org.sunbird.common.Platform
 import org.sunbird.common.dto.Request
 import org.sunbird.common.exception.ClientException
@@ -11,11 +12,11 @@ import scala.collection.mutable.ListBuffer
 
 object CategoryManager {
 
+  private val languageCodes = Platform.getStringList("platform.language.codes", new util.ArrayList[String]())
   def validateTranslationMap(request: Request) = {
-    val translations: util.Map[String, AnyRef] = Optional.ofNullable(request.get("translations").asInstanceOf[util.HashMap[String, AnyRef]]).orElse(new util.HashMap[String, AnyRef]())
+    val translations: util.Map[String, AnyRef] = request.getOrElse("translations", "").asInstanceOf[util.HashMap[String, AnyRef]]
     if (translations.isEmpty) request.getRequest.remove("translations")
     else {
-      val languageCodes = Platform.getStringList("platform.language.codes", new util.ArrayList[String]())
       if (translations.asScala.exists(entry => !languageCodes.contains(entry._1)))
         throw new ClientException("ERR_INVALID_LANGUAGE_CODE", "Please Provide Valid Language Code For translations. Valid Language Codes are : " + languageCodes)
     }
