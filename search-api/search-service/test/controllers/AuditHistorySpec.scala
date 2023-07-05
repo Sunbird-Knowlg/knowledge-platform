54b4b819e3e3f377d03e16f038f28a210c4a2591
Aiman Sharief <aiman.sharief.17@gmail.com>
2023-07-05 17:12:57 +0530

    Issue KN-10 fix: Added Test case in AuditHistorySpec

--- search-api/search-service/test/controllers/AuditHistorySpec.scala
+++ search-api/search-service/test/controllers/AuditHistorySpec.scala
@@ -17,6 +17,29 @@ class AuditHistorySpec extends BaseSpec {
       status(response) must equalTo(OK)
     }
 
+    "return success response with allFields" in {
+      val controller = app.injector.instanceOf[controllers.AuditHistoryController]
+      val fields = controller.setSearchCriteria("3.0", true)
+
+      fields.contains("logRecord") must beTrue
+      fields.contains("summary") must beTrue
+    }
+
+    "return fields without 'summary' for versionId 3.0" in {
+      val controller = app.injector.instanceOf[controllers.AuditHistoryController]
+      val fields = controller.setSearchCriteria("3.0", false)
+
+      fields.contains("logRecord") must beTrue
+      fields.contains("summary") must beFalse
+    }
+
+    "return fields with 'summary' for versionId other than 3.0" in {
+      val controller = app.injector.instanceOf[controllers.AuditHistoryController]
+      val fields = controller.setSearchCriteria("2.0", false)
+
+      fields.contains("logRecord") must beFalse
+      fields.contains("summary") must beTrue
+    }
 
   }
 }
