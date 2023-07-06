e53a9e479b7181b39ba1ec54a5eac8e6e5c018a9
Aiman Sharief <aiman.sharief.17@gmail.com>
2023-07-06 10:57:15 +0530

    Issue KN-10 fix: Added Test cases in AuditHistoryActorTest

--- search-api/search-actors/src/test/java/org/sunbird/actors/AuditHistoryActorTest.java
+++ search-api/search-actors/src/test/java/org/sunbird/actors/AuditHistoryActorTest.java
@@ -7,6 +7,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.sunbird.common.dto.Request;
 import org.sunbird.common.dto.Response;
+import org.sunbird.graph.common.enums.AuditProperties;
 import org.sunbird.search.client.ElasticSearchUtil;
 import org.sunbird.search.util.SearchConstants;
 import scala.concurrent.duration.Duration;
@@ -38,8 +39,8 @@ public class AuditHistoryActorTest extends SearchBaseActorTest{
         Request request = getAuditRequest();
         Map<String, Object> filters = new HashMap<String, Object>();
         Map<String, Object> sort = new HashMap<String, Object>();
-        sort.put("createdOn", "desc");
-        sort.put("operation", "desc");
+        sort.put(AuditProperties.createdOn.name(), "desc");
+        sort.put(AuditProperties.operation.name(), "desc");
         List<String> fields = new ArrayList<String>();
         fields.add("audit_id");
         fields.add("label");
@@ -62,12 +63,47 @@ public class AuditHistoryActorTest extends SearchBaseActorTest{
         request.getContext().put( "ENV","search");
         Response response = getAuditResponse(request);
         Map<String, Object> result = response.getResult();
-        Map<String, Object> auditHistoryRecord = (Map<String, Object>) result.get("audit_history_record");
+        Map<String, Object> auditHistoryRecord = (Map<String, Object>) result.get(AuditProperties.audit_history_record.name());
         Assert.assertNotNull(auditHistoryRecord);
         int count = (int) auditHistoryRecord.get("count");
         Assert.assertTrue(count > 1);
         List<Map<String, Object>> results = (List<Map<String, Object>>) auditHistoryRecord.get("results");
         Assert.assertNotNull(results);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testForNullGraphId(){
+        Request request = getAuditRequest();
+        Map<String, Object> filters = new HashMap<String, Object>();
+        Map<String, Object> sort = new HashMap<String, Object>();
+        sort.put("createdOn", "desc");
+        sort.put("operation", "desc");
+        List<String> fields = new ArrayList<String>();
+        fields.add("audit_id");
+        fields.add("label");
+        fields.add("objectId");
+        fields.add("objectType");
+        fields.add("operation");
+        fields.add("requestId");
+        fields.add("userId");
+        fields.add("graphId");
+        fields.add("createdOn");
+        fields.add("logRecord");
+        filters.put("graph_id","");
+        filters.put("objectId","1234");
+        request.put("filters", filters);
+        request.put("sort_by", sort);
+        request.put("traversal", traversal);
+        request.put("fields", fields);
+        request.put("ACTOR","learning.platform");
+        request.getContext().put("CHANNEL_ID","in.ekstep");
+        request.getContext().put( "ENV","search");
+        Response response = getAuditResponse(request);
+        Map<String, Object> result = response.getResult();
+        Map<String, Object> auditHistoryRecord = (Map<String, Object>) result.get("audit_history_record");
+        List<Map<String, Object>> results = (List<Map<String, Object>>) auditHistoryRecord.get("results");
+        Assert.assertTrue(results.isEmpty());
     }
 
     @SuppressWarnings("unchecked")
@@ -91,10 +127,52 @@ public class AuditHistoryActorTest extends SearchBaseActorTest{
         fields.add("logRecord");
         filters.put("graphId","domain");
         filters.put("objectId","do_113807000868651008130");
+        request.put("fuzzy",true);
+        request.put("exists","invalidExists");
+        request.put("not_exists","invalidNotExists");
         request.put("filters", filters);
         request.put("sort_by", sort);
         request.put("traversal", traversal);
         request.put("fields", fields);
+        request.put("ACTOR","learning.platform");
+        request.getContext().put("CHANNEL_ID","in.ekstep");
+        request.getContext().put( "ENV","search");
+        Response response = getAuditResponse(request);
+        Map<String, Object> result = response.getResult();
+        String message = (String) result.get("messages");
+        Assert.assertTrue(message.contains("Unable to parse data! | Error is:"));
+    }
+
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testWeightages(){
+        Request request = getAuditRequest();
+        Map<String, Object> filters = new HashMap<String, Object>();
+        Map<String, Object> sort = new HashMap<String, Object>();
+        sort.put("createdOn", "desc");
+        sort.put("operation", "desc");
+        List<String> fields = new ArrayList<String>();
+        fields.add("audit_id");
+        fields.add("label");
+        fields.add("objectId");
+        fields.add("objectType");
+        fields.add("operation");
+        fields.add("requestId");
+        fields.add("userId");
+        fields.add("graphId");
+        fields.add("createdOn");
+        fields.add("logRecord");
+        filters.put("graphId","domain");
+        filters.put("objectId","1234");
+        filters.put("objectType","Content");
+        filters.put("relatedBoards","invalidRelatedBoards");
+        request.put("fuzzy",true);
+        request.put("filters", filters);
+        request.put("sort_by", sort);
+        request.put("traversal", traversal);
+        request.put("fields", fields);
+        request.put("mode", "soft");
         request.put("ACTOR","learning.platform");
         request.getContext().put("CHANNEL_ID","in.ekstep");
         request.getContext().put( "ENV","search");
@@ -138,6 +216,60 @@ public class AuditHistoryActorTest extends SearchBaseActorTest{
         Assert.assertNotNull(auditHistoryRecord);
         Assert.assertFalse(auditHistoryRecord.containsKey("logRecord"));
     }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testInvalidValues(){
+        Request request = getAuditRequest();
+        Map<String, Object> filters = new HashMap<String, Object>();
+        Map<String, Object> sort = new HashMap<String, Object>();
+        sort.put("createdOn", "desc");
+        sort.put("operation", "desc");
+        List<String> fields = new ArrayList<String>();
+        fields.add("audit_id");
+        fields.add("label");
+        fields.add("objectId");
+        fields.add("objectType");
+        fields.add("operation");
+        fields.add("requestId");
+        fields.add("userId");
+        fields.add("graphId");
+        fields.add("createdOn");
+        fields.add("logRecord");
+        filters.put("graphId","");
+        filters.put("objectId","");
+        filters.put("tags","invalidTags");
+        request.put("filters", filters);
+        request.put("sort_by", sort);
+        request.put("traversal", traversal);
+        request.put("fields", fields);
+        request.put("ACTOR","learning.platform");
+        request.getContext().put("CHANNEL_ID","in.ekstep");
+        request.getContext().put( "ENV","search");
+        Response response = getAuditResponse(request);
+        Map<String, Object> result = response.getResult();
+        Map<String, Object> auditHistoryRecord = (Map<String, Object>) result.get("audit_history_record");
+        List<Map<String, Object>> results = (List<Map<String, Object>>) auditHistoryRecord.get("results");
+        Assert.assertTrue(results.isEmpty());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testNullValues(){
+        Request request = getAuditRequest();
+        request.put("filters", null);
+        request.put("sort_by", null);
+        request.put("traversal", null);
+        request.put("fuzzy",null );
+        request.put("fields", null);
+        request.put("ACTOR","learning.platform");
+        request.getContext().put("CHANNEL_ID","in.ekstep");
+        request.getContext().put( "ENV","search");
+        Response response = getAuditResponse(request);
+        Map<String, Object> result = response.getResult();
+        Assert.assertTrue(result.isEmpty());
+    }
+
 
     @SuppressWarnings("unchecked")
     @Test
