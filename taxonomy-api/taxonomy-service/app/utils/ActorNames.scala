2e9164c14d1b43c4f93a09be6f77f25e5624df8c
shiva-rakshith <rakshiths@sanketika.in>
2023-07-12 15:02:45 +0530

    feat: update module

--- taxonomy-api/taxonomy-service/app/utils/ActorNames.scala
+++ taxonomy-api/taxonomy-service/app/utils/ActorNames.scala
@@ -5,5 +5,9 @@ object ActorNames {
     final val HEALTH_ACTOR = "healthActor"
     final val OBJECT_CATEGORY_ACTOR = "objectCategoryActor"
     final val OBJECT_CATEGORY_DEFINITION_ACTOR = "objectCategoryDefinitionActor"
+    final val FRAMEWORK_ACTOR = "frameworkActor"
+    final val CATEGORY_ACTOR = "categoryActor"
+    final val CATEGORY_INSTANCE_ACTOR = "categoryInstanceActor"
+    final val TERM_ACTOR = "TermActor"
 
 }
