9dae24ea651de920574a253825b150e5f14d6181
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2023-07-18 15:31:37 +0530

    Issue #DBC-2023 feat: Protected Content Read

--- content-api/content-actors/src/main/scala/org/sunbird/util/AccessVerifierUtil.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/util/AccessVerifierUtil.scala
@@ -14,19 +14,23 @@ object AccessVerifierUtil {
 
   def checkAccess(accessRules: List[AnyRef], request: Request): Boolean = {
     val accessToken = request.getRequest.getOrDefault("consumerId","").asInstanceOf[String]
-    if(StringUtils.isEmpty(accessToken)) throw new ClientException("ERR_CONTENT_ACCESS_RESTRICTED", "Please provide valid user token ")
+    if(StringUtils.isEmpty(accessToken)) throw new ClientException("ERR_CONTENT_ACCESS_RESTRICTED", "You are not permitted to view this content!")
 
-    val userPayload: Map[String, AnyRef] = AccessTokenValidator.verifyUserToken(accessToken, request.getContext).asScala.toMap[String, AnyRef]
+    try {
+      val userPayload: Map[String, AnyRef] = AccessTokenValidator.verifyUserToken(accessToken, request.getContext).asScala.toMap[String, AnyRef]
 
-    val passedRules = accessRules.filter(accessCondition => {
-      val convertedAccessCondition = accessCondition.asInstanceOf[java.util.HashMap[String, AnyRef]].asScala.toMap[String, AnyRef]
-      val passedConditions =	convertedAccessCondition.filter(record => {
-        userPayload.contains(record._1) && record._2.asInstanceOf[util.ArrayList[String]].asScala.toList.contains(userPayload.getOrElse(record._1,"").asInstanceOf[String])
+      val passedRules = accessRules.filter(accessCondition => {
+        val convertedAccessCondition = accessCondition.asInstanceOf[java.util.HashMap[String, AnyRef]].asScala.toMap[String, AnyRef]
+        val passedConditions =	convertedAccessCondition.filter(record => {
+          userPayload.contains(record._1) && record._2.asInstanceOf[util.ArrayList[String]].asScala.toList.contains(userPayload.getOrElse(record._1,"").asInstanceOf[String])
+        })
+        if(passedConditions.size == convertedAccessCondition.size) true else false
       })
-      if(passedConditions.size == convertedAccessCondition.size) true else false
-    })
 
-    if(passedRules.nonEmpty) true else false
+      if(passedRules.nonEmpty) true else false
+    } catch {
+      case e: ClientException => throw new ClientException("ERR_CONTENT_ACCESS_RESTRICTED", "You are not permitted to view this content!")
+    }
   }
 
 
