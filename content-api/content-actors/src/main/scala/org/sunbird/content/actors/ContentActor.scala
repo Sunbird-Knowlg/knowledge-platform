18e4823edd681a66db4b0b8e07e7fd8fbc06c74b
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2023-07-19 15:19:21 +0530

    Issue #DBC-2023 feat: Protected Content Read

--- content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
@@ -341,16 +341,4 @@ class ContentActor @Inject() (implicit oec: OntologyEngineContext, ss: StorageSe
 		}).flatMap(f => f)
 	}
 
-	private def checkAccess(accessRules: List[AnyRef], userPayload: Map[String, AnyRef]): Boolean = {
-		val passedRules = accessRules.filter(accessCondition => {
-			val convertedAccessCondition = accessCondition.asInstanceOf[java.util.HashMap[String, AnyRef]].asScala.toMap[String, AnyRef]
-			val passedConditions =	convertedAccessCondition.filter(record => {
-				userPayload.contains(record._1) && record._2.asInstanceOf[util.ArrayList[String]].asScala.toList.contains(userPayload.getOrElse(record._1,"").asInstanceOf[String])
-			})
-			if(passedConditions.size == convertedAccessCondition.size) true else false
-		})
-
-		if(passedRules.nonEmpty) true else false
-	}
-
 }
