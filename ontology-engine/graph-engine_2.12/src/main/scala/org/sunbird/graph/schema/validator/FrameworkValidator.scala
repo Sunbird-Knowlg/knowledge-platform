92a3343efb7fb27eb114316097dd029d381338fb
Jayaprakash8887 <jayaprakash.n@tarento.com>
2023-07-19 18:40:14 +0530

    Update FrameworkValidator.scala

--- ontology-engine/graph-engine_2.12/src/main/scala/org/sunbird/graph/schema/validator/FrameworkValidator.scala
+++ ontology-engine/graph-engine_2.12/src/main/scala/org/sunbird/graph/schema/validator/FrameworkValidator.scala
@@ -155,13 +155,13 @@ trait FrameworkValidator extends IDefinition {
 
   private def getValidatedTerms(node: Node, validationList: List[String])(implicit ec: ExecutionContext, oec: OntologyEngineContext): Future[Map[String, AnyRef]] = {
     val ids: List[String] = node.getMetadata.asScala
-        .filter(entry => validationList.contains(entry._1))
-        .flatMap(entry => entry._2 match {
-          case e: String => List(e)
-          case e: util.List[String] => e.asScala
-          case e: Array[String] => e.toList
-          case _ => List()
-        }).toList
+          .filter(entry => validationList.contains(entry._1))
+          .flatMap(entry => entry._2 match {
+            case e: String => List(e)
+            case e: util.List[String] => e.asScala
+            case e: Array[String] => e.toList
+            case _ => List()
+          }).toList
     if (ids.nonEmpty) {
       val mc: MetadataCriterion = MetadataCriterion.create(new util.ArrayList[Filter]() {
         {
