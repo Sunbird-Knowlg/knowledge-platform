fb01d73ba22617e42018a466cf2621e97c0c9350
shourya-solanki <shourya.solanki@syngenta.com>
2023-07-27 15:56:18 +0530

    Issue #KN-885 fix: host changes

--- ontology-engine/graph-dac-api/src/test/java/org/sunbird/test/BaseTest.java
+++ ontology-engine/graph-dac-api/src/test/java/org/sunbird/test/BaseTest.java
@@ -6,7 +6,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.neo4j.driver.v1.*;
 import org.neo4j.graphdb.GraphDatabaseService;
-import java.net.InetAddress;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 //import org.neo4j.kernel.configuration.BoltConnector;
@@ -15,11 +14,9 @@ import org.sunbird.graph.service.util.DriverUtil;
 import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
-import org.testcontainers.utility.MountableFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
@@ -35,7 +32,6 @@ public class BaseTest {
 	private static FixedHostPortGenericContainer<?> neo4jContainer = new FixedHostPortGenericContainer<>("neo4j:3.5.0");
 //	private static Neo4jContainer<?> container = new Neo4jContainer<>("neo4j:3.5.0");
 	protected static Session graphDb = null;
-	static File customHostsFile = new File("src/test/resources/custom-hosts");
 
 	private static String NEO4J_SERVER_ADDRESS = "localhost:7687";
 	private static String GRAPH_DIRECTORY_PROPERTY_KEY = "graph.dir";
@@ -64,33 +60,37 @@ public class BaseTest {
 			}
 		});
 	}
-	private static void setupEmbeddedNeo4J() throws InterruptedException, UnknownHostException {
+	private static void setupEmbeddedNeo4J() throws InterruptedException {
 		if (graphDb == null) {
 			File graphDir = new File(graphDirectory);
 			if (!graphDir.exists()) {
 				graphDir.mkdirs();
 			}
-			InetAddress localHost = InetAddress.getLocalHost();
-
-			// Get the system's IP address as a string
-			String systemIpAddress = localHost.getHostAddress();
-			System.out.println("System IP Address: " + systemIpAddress);
 			neo4jContainer.withFixedExposedPort(hostHttpsPort, hostHttpsPort);
 			neo4jContainer.withFixedExposedPort(hostHttpPort, hostHttpPort);
 			neo4jContainer.withFixedExposedPort(hostBoltPort, hostBoltPort);
 			neo4jContainer.withEnv("NEO4J_dbms_directories_data", graphDirectory);
 			neo4jContainer.withEnv("NEO4J_dbms_security_auth__enabled", "false");
 			neo4jContainer.withCommand("neo4j", "console");
-			neo4jContainer.withExtraHost("extra-host", neo4jContainer.getHost());
+			neo4jContainer.withExtraHost("extra-host", "127.0.0.1");
 			neo4jContainer.withStartupTimeout(java.time.Duration.ofSeconds(60));
 			neo4jContainer.waitingFor(Wait.forListeningPort());
 			neo4jContainer.start();
+
+/*
+			container.withEnv("NEO4J_dbms_directories_data", graphDirectory);
+			container.withEnv("NEO4J_dbms_security_auth__enabled", "false");
+			container.withCommand("neo4j", "console");
+			container.withStartupTimeout(java.time.Duration.ofSeconds(60));
+			container.waitingFor(Wait.forListeningPort());
+			container.start();
+*/
 
 //			System.out.println(" neo4j host "+ 			neo4jContainer.getContainerIpAddress());
 //			System.out.println(" container info :"+ neo4jContainer.getContainerInfo().getState());
 
 			Thread.sleep(20000);
-			String boltAddress = "bolt://"+ neo4jContainer.getHost() + ":" + hostBoltPort; //container.getBoltUrl();
+			String boltAddress = "bolt://"+ "127.0.0.1" + ":" + hostBoltPort; //container.getBoltUrl();
 			Config config = Config.builder()
 					.withConnectionTimeout(30, TimeUnit.SECONDS)
 					.withMaxTransactionRetryTime(1, TimeUnit.MINUTES)
