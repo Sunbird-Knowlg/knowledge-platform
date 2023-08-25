9a479eaf0c427f55cfa7da990f496038c49d82f2
Aiman Sharief <aiman.sharief.17@gmail.com>
2023-08-25 15:05:43 +0530

    Issue KN-10 fix:Added Test case for Request Class

---
+++ platform-core/platform-common/src/test/java/org/sunbird/common/dto/RequestTest.java
@@ -0,0 +1,22 @@
+package org.sunbird.common.dto;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RequestTest {
+    @Test
+    public void testGetRequestString() {
+        Request request = new Request();
+        String key = "testKey";
+        String value = "testValue";
+        request.put(key, value);
+        String result = request.getRequestString(key, "defaultValue");
+        Assert.assertEquals(value, result);
+    }
+
+    @Test
+    public void testGetRequestStringWithDefaultValue() {
+        Request request = new Request();
+        String result = request.getRequestString("nonExistentKey", "");
+        Assert.assertEquals("", result);
+    }
+}
