50e73e3a99d2bcb822af6a4cd512c1f437d5a3e9
Kumar Gauraw <er.krgauraw@gmail.com>
2024-04-17 13:04:32 +0530

    Issue #IQ-750 fix: removed schema and service code for Question

--- assessment-api/assessment-service/app/utils/QuestionSetOperations.scala
+++
@@ -1,7 +0,0 @@
-package utils
-
-object QuestionSetOperations extends Enumeration {
-  val createQuestionSet, readQuestionSet, readPrivateQuestionSet, updateQuestionSet, reviewQuestionSet, publishQuestionSet,
-  retireQuestionSet, addQuestion, removeQuestion, updateHierarchyQuestion, readHierarchyQuestion,
-  rejectQuestionSet, importQuestionSet, systemUpdateQuestionSet, copyQuestionSet = Value
-}
