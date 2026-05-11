2d26328d79a60e6f6d2fa641ff456e95aac42c39
romit shah <romitshah02@gmail.com>
2026-05-11 18:23:54 +0530

    janusraph import fix

--- docker/data-seed/import_graph.groovy
+++ docker/data-seed/import_graph.groovy
@@ -1,2 +1,13 @@
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal
+
+// Define connection
+conn = DriverRemoteConnection.using('/opt/bitnami/janusgraph/conf/remote.yaml')
+g = traversal().withRemote(conn)
+
+// Execute the drop and read
 g.V().drop().iterate()
-g.io("/tmp/graph_snapshot.json").with(IO.graphson).read().iterate()
+g.io("/tmp/graph_snapshot.json").read().iterate()
+
+// Close the connection
+conn.close()
